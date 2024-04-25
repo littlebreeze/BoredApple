bd973ef6a31189685353fb4c12efaeeee3756eff
권정훈 <dhtmxk8134@naver.com>
2024-04-25 17:28:03 +0900

    chore(fe): 더미 페이지 생성

--- client/src/app/(intro)/quiz/result/[id]/page.tsx
+++ client/src/app/(intro)/quiz/result/[id]/page.tsx
@@ -1,3 +1,9 @@
-export default function Page() {
-  return <main className='bg-yellow-400'>quiz result 번 페이지</main>;
+export default function Page({ params }: { params: { id: string } }) {
+  return <div className='bg-yellow-400'>{params.id}번 퀴즈 결과 페이지 </div>;
+}
+
+export async function generateStaticParams() {
+  const paths = [{ id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }, { id: '5' }];
+
+  return paths;
 }
---
+++ client/src/app/(main)/home/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <div className='bg-yellow-400'>홈 페이지</div>;
+}
