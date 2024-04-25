6ff719db2628a58b37cb7d87292d1adf56fd9271
강지헌 <wlgjs0458@naver.com>
2024-04-25 10:39:10 +0900

    feat(be): app.py 기본코드 작성

--- recommend/recommend/app.py
+++ recommend/recommend/app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/hello', methods=['GET'])
 def hello_world():  # put application's code here
     return 'Hello World!'
 
