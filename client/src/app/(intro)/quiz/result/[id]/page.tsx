e4603ba0b9a0a991d408f2461a0e83d64c24539d
권정훈 <dhtmxk8134@naver.com>
2024-04-25 17:43:38 +0900

    chore(fe): 설정 파일 수정

--- client/src/app/(intro)/quiz/result/[id]/page.tsx
+++ client/src/app/(intro)/quiz/result/[id]/page.tsx
@@ -1,9 +1,3 @@
 export default function Page({ params }: { params: { id: string } }) {
   return <div className='bg-yellow-400'>{params.id}번 퀴즈 결과 페이지 </div>;
 }
-
-export async function generateStaticParams() {
-  const paths = [{ id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }, { id: '5' }];
-
-  return paths;
-}
