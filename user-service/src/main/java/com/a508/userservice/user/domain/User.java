30faf5ef808a128142f76cbe9781bfd20c755e6c
최정윤 <janet971015@naver.com>
2024-04-25 17:39:02 +0900

    fix(be): 초기파일 설정 수정

--- user-service/src/main/java/com/a508/userservice/user/domain/User.java
+++ user-service/src/main/java/com/a508/userservice/user/domain/User.java
@@ -1,7 +1,8 @@
 package com.a508.userservice.user.domain;
 
+import com.a508.userservice.common.entity.BaseEntity;
 import jakarta.persistence.Entity;
 
 @Entity
-public class User {
+public class User extends BaseEntity {
 }
