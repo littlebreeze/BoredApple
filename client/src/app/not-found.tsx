9256386c2dcd6e5ca600c660d29f16f84e180fc6
권정훈 <dhtmxk8134@naver.com>
2024-04-26 10:42:15 +0900

    chore(fe): 폰트 및 색상 커스텀 설정

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,11 +1,7 @@
 import { NextPage } from 'next';
 
 const NotFound: NextPage = () => {
-  return (
-    <div>
-      <div>존재하지 않는 페이지입니다.</div>
-    </div>
-  );
+  return <div className='bg-yellow-200'>존재하지 않는 페이지입니다.</div>;
 };
 
 export default NotFound;
