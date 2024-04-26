54d0ee00e81c4652486c645076c4644f72e484ff
이지원 <dlwldnjs950@naver.com>
2024-04-26 17:11:25 +0900

    design(fe): /about 페이지 - card 이동

--- client/src/app/(intro)/about/_component/Card.tsx
+++ client/src/app/(intro)/about/_component/Card.tsx
@@ -9,7 +9,7 @@ export default function Card({ num }: { num: number }) {
       height={500}
       data-w-id='82fb0c48-ee0d-a227-b693-79ad41998fe6'
       alt=''
-      className='w-1/4'
+      className='w-1/5 drop-shadow-[2px_2px_2px_rgba(0,0,0,0.2)]'
     />
     // <div className='w-1/3 bg-black'>
     //   <div className='bg-green-500/50 w-[200px] h-[200px]'></div>
--- client/src/app/(intro)/about/page.tsx
+++ client/src/app/(intro)/about/page.tsx
@@ -1,5 +1,6 @@
 import Header from '@/app/_common/Header';
 import Card from './_component/Card';
+import CardContainer from './_component/CardContainer';
 
 export default function Page() {
   return (
@@ -12,7 +13,7 @@ export default function Page() {
             <div className='text-white font-Ansungtangmyun text-7xl'>심심한 사과</div>
           </div>
           <div
-            className=' w-[215px] h-[60px] cursor-pointer my- bg-ourBlack flex justify-between items-center rounded-full text-white px-7 duration-[0.2s]
+            className=' w-[215px] h-[60px] cursor-pointer  bg-ourBlack flex justify-between items-center rounded-full text-white px-7 duration-[0.2s]
           hover:bg-ourBlue'
           >
             <div>시작하기</div>
@@ -20,7 +21,7 @@ export default function Page() {
           </div>
         </div>
         {/* 움직일 div */}
-        <div className='w-full block mt-[104px] mb-[60px]'>
+        <CardContainer>
           {/* 카드를 감싼 div */}
           <div className='flex flex-row gap-[15px] justify-start items-stretch ml-[15px]'>
             <Card num={1} />
@@ -28,8 +29,13 @@ export default function Page() {
             <Card num={3} />
             <Card num={4} />
             <Card num={5} />
+            <Card num={1} />
+            <Card num={2} />
+            <Card num={3} />
+            <Card num={4} />
+            <Card num={5} />
           </div>
-        </div>
+        </CardContainer>
       </div>
       <div className='bg-ourBlue h-[400px]'>b</div>
       <div className='bg-green-200'>c</div>
