00f0cd5e116a8304d7bc56de29f99e6b43ac03d7
이지원 <dlwldnjs950@naver.com>
2024-04-29 14:13:05 +0900

    design(fe): 소개페이지 default경로, hover 효과

--- client/src/app/(intro)/(about)/page.tsx
+++ client/src/app/(intro)/(about)/page.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import Header from '@/app/_common/Header';
 import Card from './_component/Card';
 import CardContainer from './_component/CardContainer';
+import SituationCard from './_component/SituationCard';
 
 export const metadata: Metadata = {
   title: '심심한 사과, 당신의 문해력 지키미',
@@ -10,7 +11,7 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <div>
+    <div className='bg-white'>
       <Header />
       <div className='bg-ourTheme pt-[140px] pb-[50px] overflow-hidden'>
         <div className='flex flex-col items-center text-center gap-9'>
@@ -43,8 +44,26 @@ export default function Page() {
           </div>
         </CardContainer>
       </div>
-      <div className='bg-ourBlue h-[400px]'>b</div>
-      <div className='bg-green-200'>c</div>
+      <div className='py-10 bg-ourBlue/5'>
+        <div className='text-center'>
+          <div>다음과 같은 상황,</div>
+          <div>한번쯤 겪어보지 않으셨나요?</div>
+        </div>
+        <div className='relative top-16 ping flex flex-col gap-5 justify-center px-5 md:px-28 md:flex-row'>
+          <SituationCard />
+          <SituationCard />
+          <SituationCard />
+          <SituationCard />
+        </div>
+      </div>
+      <div className='upside'>위로스르륵</div>
+      <div className='beside'>좌우스르륵</div>
+      <div className='upside'>위로스르륵</div>
+      <div className='beside'>좌우스르륵</div>
+      <div className='upside'>위로스르륵</div>
+      <div className='upside'>단체도입 배경색</div>
+      <div className='ping'>업적 - 호버 디용</div>
+      <div className='upside'>자주 묻는 질문</div>
     </div>
   );
 }
