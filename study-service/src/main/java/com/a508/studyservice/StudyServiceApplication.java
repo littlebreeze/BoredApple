1569adc4e0a016e94bfe64226787fc6031b88a0d
최정윤 <janet971015@naver.com>
2024-04-29 14:43:22 +0900

    init project

---
+++ study-service/src/main/java/com/a508/studyservice/StudyServiceApplication.java
@@ -0,0 +1,15 @@
+package com.a508.studyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class StudyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyServiceApplication.class, args);
+	}
+
+}
