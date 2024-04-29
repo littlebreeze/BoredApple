149c44e7463c427c213fd92fe909d3464db9e088
최정윤 <janet971015@naver.com>
2024-04-29 10:42:53 +0900

    feat(be): token->memberId api 구현

--- user-service/src/main/java/com/a508/userservice/user/controller/UserController.java
+++ user-service/src/main/java/com/a508/userservice/user/controller/UserController.java
@@ -1,7 +1,18 @@
 package com.a508.userservice.user.controller;
 
+import com.a508.userservice.common.jwt.TokenProvider;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final TokenProvider tokenProvider;
+    @GetMapping("userId")
+    public Integer getUserIdByToken(HttpServletRequest request) {
+        return tokenProvider.getMemberIdByToken(request);
+    }
 }
