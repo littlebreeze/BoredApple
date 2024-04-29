999c01e4663772779d9d65e2448cc4536f1197d3
강지헌 <wlgjs0458@naver.com>
2024-04-29 14:23:22 +0900

    init mypage

---
+++ mypage-service/src/main/java/com/a508/mypageservice/MypageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.a508.mypageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypageServiceApplication.class, args);
+	}
+
+}
---
+++ mypage-service/src/test/java/com/a508/mypageservice/MypageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a508.mypageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
