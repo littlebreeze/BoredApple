73773bf725e240a6cb9df89e96ebba7fb4d146b0
권정훈 <dhtmxk8134@naver.com>
2024-04-29 14:35:54 +0900

    chore(fe): 폴더명 수정

---
+++ client/src/app/(intro)/quiz/[id]/page.tsx
@@ -0,0 +1,3 @@
+export default function Page({ params }: { params: { id: string } }) {
+  return <div className='bg-yellow-400'>{params.id}번 퀴즈 페이지 </div>;
+}
--- client/src/app/example/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Example() {
-  return <main className='bg-yellow-400'>예시</main>;
-}
