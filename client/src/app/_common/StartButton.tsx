0101a4323cb262c54b8b3c45adc199674bebece5
이지원 <dlwldnjs950@naver.com>
2024-04-30 15:47:02 +0900

    feat(fe): 페이지 이동

--- client/src/app/_common/StartButton.tsx
+++ client/src/app/_common/StartButton.tsx
@@ -1,10 +1,16 @@
+import Link from 'next/link';
+
 export default function StartButton() {
   return (
-    <div
-      className='px-7 py-2 cursor-pointer bg-ourBlack rounded-full text-white duration-[0.2s]
+    <>
+      <Link href={'/home'}>
+        <div
+          className='px-7 py-2 cursor-pointer bg-ourBlack rounded-full text-white duration-[0.2s]
           hover:bg-ourBlack/60'
-    >
-      <button>시작하기</button>
-    </div>
+        >
+          <button>시작하기</button>
+        </div>
+      </Link>
+    </>
   );
 }
