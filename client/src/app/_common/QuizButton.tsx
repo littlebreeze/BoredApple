d27ffdbec206f90f900f9e15f8d56a6f1d18de56
이지원 <dlwldnjs950@naver.com>
2024-04-30 15:42:26 +0900

    design(fe): 소개페이지 헤더 변경

--- client/src/app/_common/QuizButton.tsx
+++ client/src/app/_common/QuizButton.tsx
@@ -1,6 +1,9 @@
 export default function QuizButton() {
   return (
-    <div>
+    <div
+      className='px-7 py-2 cursor-pointer bg-ourBlue rounded-full text-white duration-[0.2s]
+          hover:bg-ourBlue/60'
+    >
       <button>간편 테스트</button>
     </div>
   );
--- client/src/app/_common/StartButton.tsx
+++ client/src/app/_common/StartButton.tsx
@@ -1,6 +1,9 @@
 export default function StartButton() {
   return (
-    <div>
+    <div
+      className='px-7 py-2 cursor-pointer bg-ourBlack rounded-full text-white duration-[0.2s]
+          hover:bg-ourBlack/60'
+    >
       <button>시작하기</button>
     </div>
   );
