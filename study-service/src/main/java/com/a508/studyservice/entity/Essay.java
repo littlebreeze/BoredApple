ccb6d1ac68b9657f9bb55e0b6cd0c58eeeaf3731
이종재 <149658209+JongJae2@users.noreply.github.com>
2024-05-01 13:37:22 +0900

    feat-BE : 오늘의 학습, 주관식, 객관식, 오답 정답 확인

---
+++ study-service/src/main/java/com/a508/studyservice/entity/Essay.java
@@ -0,0 +1,35 @@
+package com.a508.studyservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+public class Essay {
+    //주관식 푼 거
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer userId;
+
+    private String myAnswer;
+
+    private String answer;
+
+    private double similarity;
+
+    private String category;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
