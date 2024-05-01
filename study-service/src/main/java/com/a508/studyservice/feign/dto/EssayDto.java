7b7ecfd745bed2ec27e070e6abc049844b9f83e5
이종재 <ljj452@naver.com>
2024-05-01 19:27:26 +0900

    feat-BE : 주관식 feignDto 생성

---
+++ study-service/src/main/java/com/a508/studyservice/feign/dto/EssayDto.java
@@ -0,0 +1,14 @@
+package com.a508.studyservice.feign.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EssayDto {
+    String myAnswer;
+    String answer;
+}
