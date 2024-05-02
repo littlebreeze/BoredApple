9867d6302c7de8a8eebd3f73b78f02b3891b588c
이종재 <ljj452@naver.com>
2024-05-03 00:47:01 +0900

    feat(be):  해결 문제 res 생성

---
+++ study-service/src/main/java/com/a508/studyservice/dto/response/ChoiceSolvedResponse.java
@@ -0,0 +1,29 @@
+package com.a508.studyservice.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChoiceSolvedResponse {
+
+    private Integer userAnswer;
+
+    private Integer answer;
+
+    //어떤 유형 문제인지
+    private String category;
+
+    private Integer problemId;
+
+    private boolean correct;
+
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
