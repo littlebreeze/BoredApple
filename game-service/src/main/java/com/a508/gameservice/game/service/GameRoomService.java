127176368b6b639930ca2c10ec99336bbdc39633
최정윤 <janet971015@naver.com>
2024-05-02 17:10:53 +0900

    feat(be): 방만들기, 방 조회 api 구현

---
+++ game-service/src/main/java/com/a508/gameservice/game/service/GameRoomService.java
@@ -0,0 +1,57 @@
+package com.a508.gameservice.game.service;
+
+import com.a508.gameservice.game.data.GameRoomReq;
+import com.a508.gameservice.game.data.GameRoomRes;
+import com.a508.gameservice.game.domain.GameRoom;
+import com.a508.gameservice.game.repository.GameRoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GameRoomService {
+
+    private final GameRoomRepository gameRoomRepository;
+
+    public List<GameRoomRes> getRooms(int pageNum) {
+        Pageable pageable = PageRequest.of(pageNum, 2, Sort.by("id").ascending());
+        Page<GameRoom> gameRoomList = gameRoomRepository.findAll(pageable);
+
+        List<GameRoomRes> gameRoomResList = new ArrayList<>();
+        for (GameRoom gameRoom : gameRoomList.getContent()) {
+            GameRoomRes gameRoomRes = GameRoomRes.builder()
+                    .id(gameRoom.getId())
+                    .roomName(gameRoom.getRoomName())
+                    .isSecret(gameRoom.getIsSecret())
+                    .roomPassword(gameRoom.getRoomPassword())
+//                    .roomCreatorId()
+                    .maxNum(gameRoom.getMaxNum())
+                    .quizCount(gameRoom.getQuizCount())
+                    .isStarted(gameRoom.getIsStarted())
+                    .build();
+            gameRoomResList.add(gameRoomRes);
+        }
+
+        return gameRoomResList;
+    }
+
+
+    public void createRoom(GameRoomReq gameRoomReq) {
+        GameRoom gameRoom = GameRoom.builder()
+                .id(gameRoomReq.getId())//id 가장 작은 값으로 배정 로직 만들기
+                .roomName(gameRoomReq.getRoomName())
+                .roomPassword(gameRoomReq.getRoomPassword())
+//                .roomCreatorId(gameRoomReq.getRoomCreatorId()) user-service에 요청보내기
+                .quizCount(gameRoomReq.getQuizCount())
+                .isSecret(gameRoomReq.getIsSecret())
+                .maxNum(gameRoomReq.getMaxNum()).build();
+        gameRoomRepository.save(gameRoom);
+    }
+}
