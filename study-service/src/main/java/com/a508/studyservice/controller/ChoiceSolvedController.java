f3c51f05b144ac0febf59542a7a76626382c0479
이종재 <ljj452@naver.com>
2024-05-03 00:44:59 +0900

    add(be): api 컨트롤러 생성

---
+++ study-service/src/main/java/com/a508/studyservice/controller/ChoiceSolvedController.java
@@ -0,0 +1,8 @@
+package com.a508.studyservice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/study-service")
+public class ChoiceSolvedController {
+}
