6e092482d5c3c440c37e9aca7cc06555a8a869d9
이종재 <ljj452@naver.com>
2024-05-03 00:41:03 +0900

    feat(be): 오늘의 학습 Response 생성

---
+++ study-service/src/main/java/com/a508/studyservice/dto/response/TodayLearningResponse.java
@@ -0,0 +1,21 @@
+package com.a508.studyservice.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TodayLearningResponse {
+
+    private String type;
+
+    private String category;
+
+
+}
