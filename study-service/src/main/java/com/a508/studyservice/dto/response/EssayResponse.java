b62bd2a2d984a06795b6430aabdf897c76a00dfb
이종재 <ljj452@naver.com>
2024-05-03 00:46:21 +0900

    feat(be):  주관식 Response 생성

---
+++ study-service/src/main/java/com/a508/studyservice/dto/response/EssayResponse.java
@@ -0,0 +1,19 @@
+package com.a508.studyservice.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EssayResponse {
+
+    String myAnswer;
+
+    String answer;
+
+    String similarity;
+
+
+}
