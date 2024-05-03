d1e5d07d67bcb94c51cf7da8ec9f739f38ce2ca0
이종재 <ljj452@naver.com>
2024-05-03 17:51:19 +0900

    feat(be): Service Interface 구현

--- study-service/src/main/java/com/a508/studyservice/service/EssayService.java
+++ study-service/src/main/java/com/a508/studyservice/service/EssayService.java
@@ -1,6 +1,14 @@
 package com.a508.studyservice.service;
 
+import com.a508.studyservice.dto.request.EssayRequest;
+import com.a508.studyservice.dto.response.EssayResponse;
+
 public interface EssayService {
+
+
+	EssayResponse getResultEssay(EssayRequest essayRequest);
+
+
 
 
 }
--- study-service/src/main/java/com/a508/studyservice/service/IntensiveService.java
+++ study-service/src/main/java/com/a508/studyservice/service/IntensiveService.java
@@ -1,4 +1,15 @@
 package com.a508.studyservice.service;
 
+import java.util.List;
+
+import com.a508.studyservice.dto.request.ProblemRequest;
+import com.a508.studyservice.dto.response.ProblemResponse;
+
 public interface IntensiveService {
+
+	List<ProblemResponse> getIntensiveProblems(String token, ProblemRequest problemRequest);
+
+	List<ProblemResponse> getTodayIntensive(String token);
+
+
 }
