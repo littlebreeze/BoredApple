d2f783cbae3fcefe7af66a112ed33b46f86f45cf
최정윤 <janet971015@naver.com>
2024-05-03 13:27:05 +0900

    feat(be): 방번호 작은 수 배정 로직 추가

--- game-service/src/main/java/com/a508/gameservice/game/service/GameRoomService.java
+++ game-service/src/main/java/com/a508/gameservice/game/service/GameRoomService.java
@@ -4,6 +4,7 @@ import com.a508.gameservice.game.data.GameRoomReq;
 import com.a508.gameservice.game.data.GameRoomRes;
 import com.a508.gameservice.game.domain.GameRoom;
 import com.a508.gameservice.game.repository.GameRoomRepository;
+import com.a508.gameservice.game.repository.GameRoomTemplateRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -13,17 +14,20 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class GameRoomService {
 
     private final GameRoomRepository gameRoomRepository;
+    private final GameRoomTemplateRepository gameRoomTemplateRepository;
 
     public List<GameRoomRes> getRooms(int pageNum) {
-        Pageable pageable = PageRequest.of(pageNum, 2, Sort.by("id").ascending());
+        Pageable pageable = PageRequest.of(pageNum - 1, 2, Sort.by("id").ascending());
         Page<GameRoom> gameRoomList = gameRoomRepository.findAll(pageable);
-
+        Boolean isEndPage = false;
+        if (pageNum == gameRoomList.getTotalPages()) isEndPage = true;
         List<GameRoomRes> gameRoomResList = new ArrayList<>();
         for (GameRoom gameRoom : gameRoomList.getContent()) {
             GameRoomRes gameRoomRes = GameRoomRes.builder()
@@ -35,6 +39,7 @@ public class GameRoomService {
                     .maxNum(gameRoom.getMaxNum())
                     .quizCount(gameRoom.getQuizCount())
                     .isStarted(gameRoom.getIsStarted())
+                    .isEndPage(isEndPage)
                     .build();
             gameRoomResList.add(gameRoomRes);
         }
@@ -43,9 +48,9 @@ public class GameRoomService {
     }
 
 
-    public void createRoom(GameRoomReq gameRoomReq) {
+    public synchronized void createRoom(GameRoomReq gameRoomReq) {
         GameRoom gameRoom = GameRoom.builder()
-                .id(gameRoomReq.getId())//id 가장 작은 값으로 배정 로직 만들기
+                .id(String.valueOf(getMinRoomId()))//id 가장 작은 값으로 배정 로직 만들기
                 .roomName(gameRoomReq.getRoomName())
                 .roomPassword(gameRoomReq.getRoomPassword())
 //                .roomCreatorId(gameRoomReq.getRoomCreatorId()) user-service에 요청보내기
@@ -53,5 +58,15 @@ public class GameRoomService {
                 .isSecret(gameRoomReq.getIsSecret())
                 .maxNum(gameRoomReq.getMaxNum()).build();
         gameRoomRepository.save(gameRoom);
+    }
+
+
+    public Integer getMinRoomId() {
+        Set<Integer> idList = gameRoomTemplateRepository.getAllIds();
+        int num = 1;
+        while (true) {
+            if (!idList.contains(num)) return num;
+            num += 1;
+        }
     }
 }
