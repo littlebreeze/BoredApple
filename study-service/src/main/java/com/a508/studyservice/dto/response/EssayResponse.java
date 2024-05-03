13bbd236143b3341232a88e164f242ae9c4de98a
이종재 <ljj452@naver.com>
2024-05-03 17:50:56 +0900

    fix(be): DTO 수정

--- study-service/src/main/java/com/a508/studyservice/dto/response/EssayResponse.java
+++ study-service/src/main/java/com/a508/studyservice/dto/response/EssayResponse.java
@@ -1,5 +1,7 @@
 package com.a508.studyservice.dto.response;
 
+import java.time.LocalDateTime;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,11 +11,24 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class EssayResponse {
 
-    String myAnswer;
+    private String title;
 
-    String answer;
+    private String question;
 
-    String similarity;
+    private String text;
+
+    private Integer userAnswer;
+
+    private Integer answer;
+
+    //어떤 유형 문제인지
+    private String category;
+
+    private Integer problemId;
+
+    private Integer similarity;
+
+    private LocalDateTime createdAt;
 
 
 }
