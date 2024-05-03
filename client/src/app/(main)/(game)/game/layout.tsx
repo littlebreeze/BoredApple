33ebcdf3942720126aeacfd67c6e65090dac4495
이지원 <dlwldnjs950@naver.com>
2024-05-03 18:06:50 +0900

    design(fe): 한지 배경

--- client/src/app/(main)/(game)/game/layout.tsx
+++ client/src/app/(main)/(game)/game/layout.tsx
@@ -7,25 +7,29 @@ export default function Layout({
 }>) {
   return (
     <>
-      {/* <Image
-        className='absolute object-cover w-full top-0 left-0 z-10 opacity-50'
-        src='game-background.svg'
-        loading='eager'
-        fill
-        alt='게임배경'
-      /> */}
-      <div className='px-5 '>
-        <div className='w-[300px] mx-auto mt-7 mb-3'>
-          <Image
-            className='w-full'
-            src='game-title-2.svg'
-            loading='eager'
-            width={500}
-            height={500}
-            alt='어휘퀴즈 타이틀'
-          />
+      <div className=''>
+        <Image
+          className='absolute object-cover h-screen top-0 left-0 opacity-50'
+          src='/game-background.svg'
+          loading='eager'
+          layout='fill'
+          alt='게임배경'
+        />
+        <div className='relative z-10'>
+          <div className='px-5'>
+            <div className='w-[300px] mx-auto pt-7 pb-3'>
+              <Image
+                className='w-full'
+                src='/game-title-2.svg'
+                loading='eager'
+                width={500}
+                height={500}
+                alt='어휘퀴즈 타이틀'
+              />
+            </div>
+            {children}
+          </div>
         </div>
-        {children}
       </div>
     </>
   );
