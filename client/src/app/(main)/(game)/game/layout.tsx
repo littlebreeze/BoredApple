ddefcf3e90b1e6e67f8f26298141e10ff9c0c50a
이지원 <dlwldnjs950@naver.com>
2024-05-03 16:45:13 +0900

    design(fe): 게임 레이아웃

--- client/src/app/(main)/(game)/game/layout.tsx
+++ client/src/app/(main)/(game)/game/layout.tsx
@@ -6,18 +6,27 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div lang='ko' className='px-5 h-screen overflow-hidden'>
-      <div className='w-[300px] mx-auto mt-7 mb-3'>
-        <Image
-          className='w-full'
-          src='game-title-2.svg'
-          loading='eager'
-          width={500}
-          height={500}
-          alt='어휘퀴즈 타이틀'
-        />
+    <>
+      {/* <Image
+        className='absolute object-cover w-full top-0 left-0 z-10 opacity-50'
+        src='game-background.svg'
+        loading='eager'
+        fill
+        alt='게임배경'
+      /> */}
+      <div className='px-5 '>
+        <div className='w-[300px] mx-auto mt-7 mb-3'>
+          <Image
+            className='w-full'
+            src='game-title-2.svg'
+            loading='eager'
+            width={500}
+            height={500}
+            alt='어휘퀴즈 타이틀'
+          />
+        </div>
+        {children}
       </div>
-      {children}
-    </div>
+    </>
   );
 }
