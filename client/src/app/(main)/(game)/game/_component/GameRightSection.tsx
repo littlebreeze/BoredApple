b0e4cfa42ea1c8d49bf777960776d73678120eaf
이지원 <dlwldnjs950@naver.com>
2024-05-03 18:50:58 +0900

    design(fe): 방 목록 레이아웃

--- client/src/app/(main)/(game)/game/_component/GameRightSection.tsx
+++ client/src/app/(main)/(game)/game/_component/GameRightSection.tsx
@@ -25,16 +25,16 @@ const roomInfo: RoomInfo = {
 export default function GameRightSection() {
   return (
     <>
-      <div className='flex flex-row justify-between mb-3'>
-        <div className='flex flex-row w-2/4 ml-2 lg:px-7 px-3 py-4 gap-4 lg:gap-7 bg-ourGray/50 rounded-xl'>
+      <div className='flex flex-row justify-between mb-2'>
+        <div className='flex flex-row w-2/4 lg:w-2/5 ml-2 px-3 py-3 gap-4 lg:px-5 lg:gap-5 bg-ourGray/50 rounded-xl'>
           <GameMenuBtn />
           <GameMenuBtn />
         </div>
-        <div className='w-1/4 lg:px-7 px-4 py-4 mr-2 bg-ourGray/50 rounded-xl'>
+        <div className='w-1/4 lg:w-1/5 lg:px-5 px-4 py-3 mr-2 bg-ourGray/50 rounded-xl'>
           <GameMenuBtn />
         </div>
       </div>
-      <div className='grid grid-cols-2 py-5 px-5 lg:px-10 gap-x-2 gap-y-3 md:gap-x-5 md:gap-y-6 lg:gap-x-10 bg-ourGray/50 rounded-xl'>
+      <div className='grid grid-cols-2 py-4 px-5 gap-x-2 gap-y-2 md:gap-x-5 md:gap-y-6 lg:gap-x-6 lg:px-6 bg-ourGray/50 rounded-xl'>
         <GameRoomItem roomInfo={roomInfo} />
         <GameRoomItem roomInfo={roomInfo} />
         <GameRoomItem roomInfo={roomInfo} />
@@ -42,7 +42,7 @@ export default function GameRightSection() {
         <GameRoomItem roomInfo={roomInfo} />
         <GameRoomItem roomInfo={roomInfo} />
       </div>
-      <div className='flex flex-row py-4 gap-1 px-8 md:gap-3 md:px-8 lg:gap-8 lg:px-20'>
+      <div className='flex flex-row py-4 gap-1 px-8 md:gap-3 md:px-8 lg:gap-6 lg:px-20'>
         <PagingBtn />
         <PagingBtn />
       </div>
--- client/src/app/(main)/(game)/game/layout.tsx
+++ client/src/app/(main)/(game)/game/layout.tsx
@@ -7,28 +7,26 @@ export default function Layout({
 }>) {
   return (
     <>
-      <div className=''>
-        <Image
-          className='absolute object-cover h-screen top-0 left-0 opacity-50'
-          src='/game-background.svg'
-          loading='eager'
-          layout='fill'
-          alt='게임배경'
-        />
-        <div className='relative z-10'>
-          <div className='px-5'>
-            <div className='w-[300px] mx-auto pt-7 pb-3'>
-              <Image
-                className='w-full'
-                src='/game-title-2.svg'
-                loading='eager'
-                width={500}
-                height={500}
-                alt='어휘퀴즈 타이틀'
-              />
-            </div>
-            {children}
+      <Image
+        className='absolute object-cover h-screen top-0 left-0 opacity-50'
+        src='/game-background.svg'
+        loading='eager'
+        layout='fill'
+        alt='게임배경'
+      />
+      <div className='relative z-10'>
+        <div className='px-5'>
+          <div className='w-[300px] mx-auto pt-7 pb-3'>
+            <Image
+              className='w-full drop-shadow-[2px_2px_2px_rgba(0,0,0,0.2)]'
+              src='/game-title-2.svg'
+              loading='eager'
+              width={500}
+              height={500}
+              alt='어휘퀴즈 타이틀'
+            />
           </div>
+          {children}
         </div>
       </div>
     </>
