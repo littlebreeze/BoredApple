370d89583e03ff0fe3de9a72db8e84823565ef4e
이종재 <ljj452@naver.com>
2024-05-03 17:48:19 +0900

    rename(be): 디렉토리 이동

--- study-service/src/main/java/com/a508/studyservice/repository/IntensiveRepository.java
+++ study-service/src/main/java/com/a508/studyservice/repository/IntensiveRepository.java
@@ -1,4 +1,4 @@
-package com.a508.studyservice.respository;
+package com.a508.studyservice.repository;
 
 import com.a508.studyservice.entity.Intensive;
 import org.springframework.data.jpa.repository.JpaRepository;
