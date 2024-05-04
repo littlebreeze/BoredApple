56910c58b3c7cbb966c2551b8822fa6723f3358b
이종재 <ljj452@naver.com>
2024-05-05 01:55:15 +0900

    feat(be):  맞춘 문제 메소드 생성

--- study-service/src/main/java/com/a508/studyservice/service/ChoiceSolvedServiceImpl.java
+++ study-service/src/main/java/com/a508/studyservice/service/ChoiceSolvedServiceImpl.java
@@ -1,19 +1,31 @@
 package com.a508.studyservice.service;
 
 
+import com.a508.studyservice.dto.response.ChoiceSolvedResponse;
 import com.a508.studyservice.repository.ChoiceRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Transactional
 @Service
 @RequiredArgsConstructor
-public class ChoiceSolvedServiceImpl {
+public class ChoiceSolvedServiceImpl  implements  ChoiceSolvedService{
 
     private final ChoiceRepository choiceRepository;
 
 
+    @Override
+    public List<ChoiceSolvedResponse> getTodayChoice(String token) {
+        return null;
+    }
 
+    @Override
+    public List<ChoiceSolvedResponse> getSpecificChoice(String token, LocalDateTime localDateTime) {
+        return null;
+    }
 }
 
