c43f3bbabd2ebce31b528857436d6a83051f79fb
이종재 <ljj452@naver.com>
2024-05-05 01:55:15 +0900

    feat(be):  맞춘 문제 메소드 생성

--- study-service/src/main/java/com/a508/studyservice/repository/ChoiceRepository.java
+++ study-service/src/main/java/com/a508/studyservice/repository/ChoiceRepository.java
@@ -5,7 +5,12 @@ import com.a508.studyservice.entity.ChoiceSolved;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ChoiceRepository extends JpaRepository<ChoiceSolved,Integer> {
+
+    Optional<List<ChoiceSolved>> findByUserId(Integer userId);
 
 }
