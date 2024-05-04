b1b2f7d5677196e2146bb9bdd495ba2a51011f66
이종재 <ljj452@naver.com>
2024-05-05 01:53:50 +0900

    fix(be):  엔티티 기본키 수정

--- study-service/src/main/java/com/a508/studyservice/entity/FiveAbility.java
+++ study-service/src/main/java/com/a508/studyservice/entity/FiveAbility.java
@@ -2,6 +2,9 @@ package com.a508.studyservice.entity;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,6 +17,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class FiveAbility {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer userId;
     private Integer fact;
     private Integer inference;
     private Integer voca;
