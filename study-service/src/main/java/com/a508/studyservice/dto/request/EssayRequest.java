d21f06d3c21555c2876a84dd33dba2629c61c05c
이종재 <149658209+JongJae2@users.noreply.github.com>
2024-05-06 22:07:30 +0900

    feat(be): 객관식, 주관식 RequestDto 추가

---
+++ study-service/src/main/java/com/a508/studyservice/dto/request/EssayRequest.java
@@ -0,0 +1,15 @@
+package com.a508.studyservice.dto.request;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class EssayRequest {
+    String type;
+
+    List<String> myAnswer;
+
+    List<Integer> problemId;
+
+}
