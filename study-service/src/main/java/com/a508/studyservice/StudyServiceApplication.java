3361bf903e5d8fedaf4f71d7c2efb8071df55e63
이종재 <149658209+JongJae2@users.noreply.github.com>
2024-05-06 22:11:26 +0900

    fix(be): Auditing Annotation 수정 (Jdbc -> Jpa )

--- study-service/src/main/java/com/a508/studyservice/StudyServiceApplication.java
+++ study-service/src/main/java/com/a508/studyservice/StudyServiceApplication.java
@@ -5,11 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-// @EnableDiscoveryClient
-@EnableJdbcAuditing
+ @EnableDiscoveryClient
+@EnableJpaAuditing
 @EnableScheduling
 @EnableFeignClients
 public class StudyServiceApplication {
