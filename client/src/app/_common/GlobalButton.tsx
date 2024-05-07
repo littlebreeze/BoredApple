04290a39c56357985b25bd6e4fd18101b56f0de6
권정훈 <dhtmxk8134@naver.com>
2024-05-07 13:20:10 +0900

    chore(Fe): global button 생성

--- client/src/app/_common/GlobalButton.tsx
+++ client/src/app/_common/GlobalButton.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useState } from 'react';
 
-export default function QuizButton() {
+export default function GlobalButton() {
   const [selected, setSelected] = useState<boolean>(false);
 
   const handleClick = () => {
@@ -14,7 +14,7 @@ export default function QuizButton() {
         className={`absolute bottom-2 mb-4 w-96 h-12 rounded-lg text-lg  ${
           selected ? 'bg-gray-300 cursor-not-allowed' : 'bg-ourBlue duration-[0.2s] hover:bg-ourBlue/80'
         } text-white`}
-        // onClick={}
+        onClick={handleClick}
         disabled={selected}
       >
         다음
