7c151cad2e00645b7064ce2cc127ea30720fffd8
강지헌 <wlgjs0458@naver.com>
2024-05-08 11:04:31 +0900

    feat(be): user 선호 category 테이블 추가

---
+++ user-service/src/main/java/com/a508/userservice/user/domain/UserCategory.java
@@ -0,0 +1,21 @@
+package com.a508.userservice.user.domain;
+
+import com.a508.userservice.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserCategory extends BaseEntity {
+
+	@Column(nullable = false)
+	private int userId;
+
+	@Column(nullable = false)
+	private String category;
+
+}
