16a160c3c84b94e8832ed1c399885c8d82aba277
최정윤 <janet971015@naver.com>
2024-05-08 16:02:33 +0900

    feat(be): ranking 관련 dto 생성

---
+++ game-service/src/main/java/com/a508/gameservice/game/data/TopRankingRes.java
@@ -0,0 +1,17 @@
+package com.a508.gameservice.game.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TopRankingRes {
+
+    private Integer ranking;
+    private String nickName;
+    private Integer rating;
+}
