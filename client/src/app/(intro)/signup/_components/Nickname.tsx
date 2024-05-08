1e16e386d87c255b32e64d9f2deeb99caa88e8eb
권정훈 <dhtmxk8134@naver.com>
2024-05-08 15:35:55 +0900

    feat(fe): 경로 변경

--- client/src/app/(intro)/signup/_components/Nickname.tsx
+++ client/src/app/(intro)/signup/_components/Nickname.tsx
@@ -11,7 +11,7 @@ export default function Nickname() {
 
   const createNickname = async () => {
     try {
-      await instance.post(`${process.env.NEXT_PUBLIC_API_SERVER}/user-service/nickname`, {
+      await instance.post(`${process.env.NEXT_PUBLIC_API_SERVER}/nickname`, {
         nickname: nickname,
       });
     } catch (error) {
