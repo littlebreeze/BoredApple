79a325413ff3bbf18f1fb2cca85a104d47330615
권정훈 <dhtmxk8134@naver.com>
2024-05-08 11:12:47 +0900

    feat(fe): 회원가입 기능 구현

--- client/src/app/(intro)/login/_components/GoogleLogin.tsx
+++ client/src/app/(intro)/login/_components/GoogleLogin.tsx
@@ -1,13 +1,12 @@
 'use client';
 import googleLogo from '@/../public/login/google-logo.svg';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
 
 export default function GoogleLogin() {
   const handleGoogleLogin = () => {
     window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?
-		client_id=${process.env.NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_ID}
-		&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_AUTH_REDIRECT_URI}
+		client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
+		&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI}
 		&response_type=code
 		&scope=email profile`;
   };
