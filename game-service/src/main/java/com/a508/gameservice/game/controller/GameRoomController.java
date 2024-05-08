11deb97b6e5bfb5c1741bc05deb5926a9b76de66
최정윤 <janet971015@naver.com>
2024-05-08 16:05:29 +0900

    feat(be): rankings api 구현

--- game-service/src/main/java/com/a508/gameservice/game/controller/GameRoomController.java
+++ game-service/src/main/java/com/a508/gameservice/game/controller/GameRoomController.java
@@ -2,6 +2,7 @@ package com.a508.gameservice.game.controller;
 
 import com.a508.gameservice.game.data.GameRoomReq;
 import com.a508.gameservice.game.data.GameRoomRes;
+import com.a508.gameservice.game.data.RankingRes;
 import com.a508.gameservice.game.service.GameRoomService;
 import com.a508.gameservice.response.SuccessResponse;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,6 +11,8 @@ import org.apache.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,14 +21,26 @@ public class GameRoomController {
     private final GameRoomService gameRoomService;
 
     @GetMapping("/rooms/{pageNum}")
-    public SuccessResponse<List<GameRoomRes>> getRooms(HttpServletRequest request, @PathVariable int pageNum) {
+    public SuccessResponse<List<GameRoomRes>> getRooms(@PathVariable int pageNum) {
         return new SuccessResponse<>(gameRoomService.getRooms(pageNum));
     }
 
-    @PostMapping("/room")
-    public SuccessResponse<Integer> addRoom(HttpServletRequest request, @RequestBody GameRoomReq gameRoomReq) {
-        gameRoomService.createRoom(gameRoomReq);
+    @PostMapping("/rooms")
+    public SuccessResponse<Map<String, Integer>> addRoom(HttpServletRequest request, @RequestBody GameRoomReq gameRoomReq) {
+        Map<String, Integer> data = new TreeMap<>();
+        data.put("roomId", gameRoomService.createRoom(request, gameRoomReq));
+        return new SuccessResponse<>(data);
+    }
+
+    @PostMapping("/players")
+    public SuccessResponse<Integer> addPlayer(HttpServletRequest request, @RequestBody Integer roomId) {
+        gameRoomService.createRoomPlayer(request, roomId);
         return new SuccessResponse<>(HttpStatus.SC_CREATED);
     }
 
+
+    @GetMapping("/rankings")
+    public SuccessResponse<RankingRes> getRankings(HttpServletRequest request) {
+        return new SuccessResponse<>(gameRoomService.getRankings(request));
+    }
 }
--- game-service/src/main/java/com/a508/gameservice/game/service/GameRoomService.java
+++ game-service/src/main/java/com/a508/gameservice/game/service/GameRoomService.java
@@ -1,10 +1,15 @@
 package com.a508.gameservice.game.service;
 
-import com.a508.gameservice.game.data.GameRoomReq;
-import com.a508.gameservice.game.data.GameRoomRes;
+import com.a508.gameservice.exception.CustomException;
+import com.a508.gameservice.exception.ErrorCode;
+import com.a508.gameservice.game.data.*;
+import com.a508.gameservice.game.domain.BattleRecord;
 import com.a508.gameservice.game.domain.GameRoom;
+import com.a508.gameservice.game.repository.BattleRecordRepository;
 import com.a508.gameservice.game.repository.GameRoomRepository;
 import com.a508.gameservice.game.repository.GameRoomTemplateRepository;
+import com.a508.gameservice.game.repository.RoomPlayerRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -22,7 +27,14 @@ public class GameRoomService {
 
     private final GameRoomRepository gameRoomRepository;
     private final GameRoomTemplateRepository gameRoomTemplateRepository;
+    private final RoomPlayerRepository roomPlayerRepository;
+    private final UserServiceClient userServiceClient;
+    private final BattleRecordRepository battleRecordRepository;
+    private static final String AUTHORIZATION_HEADER = "Authorization";
 
+    /**
+     * 방 전체 목록 페이지별 반환
+     */
     public List<GameRoomRes> getRooms(int pageNum) {
         Pageable pageable = PageRequest.of(pageNum - 1, 2, Sort.by("id").ascending());
         Page<GameRoom> gameRoomList = gameRoomRepository.findAll(pageable);
@@ -35,7 +47,7 @@ public class GameRoomService {
                     .roomName(gameRoom.getRoomName())
                     .isSecret(gameRoom.getIsSecret())
                     .roomPassword(gameRoom.getRoomPassword())
-//                    .roomCreatorId()
+//                    .roomCreatorName()
                     .maxNum(gameRoom.getMaxNum())
                     .quizCount(gameRoom.getQuizCount())
                     .isStarted(gameRoom.getIsStarted())
@@ -47,21 +59,33 @@ public class GameRoomService {
         return gameRoomResList;
     }
 
+    /**
+     * 방 생성
+     */
+    public synchronized Integer createRoom(HttpServletRequest request, GameRoomReq gameRoomReq) {
 
-    public synchronized void createRoom(GameRoomReq gameRoomReq) {
+        int userId = getUserId(request);
+        int roomId = getMinRoomId();
+        //방 생성
         GameRoom gameRoom = GameRoom.builder()
-                .id(String.valueOf(getMinRoomId()))//id 가장 작은 값으로 배정 로직 만들기
+                .id(String.valueOf(roomId))
                 .roomName(gameRoomReq.getRoomName())
                 .roomPassword(gameRoomReq.getRoomPassword())
-//                .roomCreatorId(gameRoomReq.getRoomCreatorId()) user-service에 요청보내기
+                .roomCreatorId(userId)
                 .quizCount(gameRoomReq.getQuizCount())
                 .isSecret(gameRoomReq.getIsSecret())
                 .isStarted(false)
                 .maxNum(gameRoomReq.getMaxNum()).build();
         gameRoomRepository.save(gameRoom);
+        //방 입장
+        roomPlayerRepository.addPlayerToRoom(String.valueOf(roomId), userId);
+
+        return roomId;
     }
 
-
+    /**
+     * 방번호 작은 값 배정
+     */
     public Integer getMinRoomId() {
         Set<Integer> idList = gameRoomTemplateRepository.getAllIds();
         int num = 1;
@@ -69,5 +93,72 @@ public class GameRoomService {
             if (!idList.contains(num)) return num;
             num += 1;
         }
+    }
+
+    /**
+     * 방에 회원넣기
+     */
+    public synchronized void createRoomPlayer(HttpServletRequest request, Integer roomId) {
+        if (roomPlayerRepository.playerCnt(String.valueOf(roomId)) < 6) {
+            roomPlayerRepository.addPlayerToRoom(String.valueOf(roomId), 1);
+        } else {
+            new CustomException(ErrorCode.PLAYER_IS_FULL_ERROR);
+        }
+    }
+
+    public RankingRes getRankings(HttpServletRequest request) {
+        int userId = getUserId(request);
+        //일단 내 랭킹이있는지 확인
+        BattleRecord myBattleRecord = battleRecordRepository.findById(userId).orElseGet(() ->
+                battleRecordRepository.save(
+                        BattleRecord.builder()
+                                .id(userId)
+                                .game(0)
+                                .rating(1500)
+                                .victory(0)
+                                .build()
+                )
+        );
+        Sort sort = Sort.by(Sort.Direction.DESC, "rating");
+        List<BattleRecord> battleRecords = battleRecordRepository.findAll(sort);
+        List<Integer> userIdList = new ArrayList<>();
+        List<TopRankingRes> rankingList = new ArrayList<>();
+        userIdList.add(userId); //0번 자리 본인
+
+        for (int i = 0; i < (battleRecords.size() > 10 ? 10 : battleRecords.size()); i++) {
+            userIdList.add(battleRecords.get(i).getId());
+        }
+
+        List<String> nicknameList = userServiceClient.getNicknameByUserId(
+                UserListReq.builder().idList(userIdList).build()
+        ).getNicknameList();
+
+        for (int i = 0; i < (battleRecords.size() > 10 ? 10 : battleRecords.size()); i++) {
+            BattleRecord battleRecord = battleRecords.get(i);
+            rankingList.add(TopRankingRes.builder()
+                    .ranking(i + 1)
+                    .rating(battleRecord.getRating())
+                    .nickName(nicknameList.get(i+1))
+                    .build());
+        }
+        Integer myRanking = -1;
+        for (int i = 0; i < (battleRecords.size() > 100 ? 100 : battleRecords.size()); i++) {
+            if (userId == battleRecords.get(i).getId()) {
+                myRanking = i + 1;
+            }
+        }
+        return RankingRes.builder()
+                .myNickName(nicknameList.get(0))
+                .myRanking(myRanking)
+                .myRating(myBattleRecord.getRating())
+                .rankingList(rankingList)
+                .build();
+
+
+    }
+
+    public int getUserId(HttpServletRequest request) {
+        String accessToken = request.getHeader(AUTHORIZATION_HEADER).substring(7);
+        return userServiceClient.getUserIdByToken(accessToken);
     }
 }
