e61d49063d719739a572935f1b36bbed97ed52fd
이지원 <dlwldnjs950@naver.com>
2024-05-08 09:26:43 +0900

    design(fe): 소개 페이지 모바일 사이즈 적용

--- client/src/app/(intro)/(about)/page.tsx
+++ client/src/app/(intro)/(about)/page.tsx
@@ -21,14 +21,14 @@ export default function Page() {
         <div className='flex flex-col items-center text-center gap-9'>
           <UpsideAnimation>
             <div className='flex flex-col gap-5'>
-              <div className='text-white text-xl'>당신의 문해력 지키미,</div>
-              <div className='text-white font-Ansungtangmyun text-7xl'>심심한 사과</div>
+              <div className='text-white text-base md:text-xl lg:text-xl'>당신의 문해력 지키미,</div>
+              <div className='text-white font-Ansungtangmyun text-5xl md:text-7xl lg:text-7xl'>심심한 사과</div>
             </div>
           </UpsideAnimation>
           <Link href={'/login'}>
             <div
-              className=' w-[215px] h-[60px] cursor-pointer bg-ourBlack flex justify-between items-center rounded-full text-white px-7 duration-[0.2s]
-          hover:bg-ourBlue'
+              className='w-[150px] h-[50px] md:w-[215px] lg:w-[215px] md:h-[60px] lg:h-[60px] cursor-pointer bg-ourBlack flex justify-between items-center rounded-full text-white px-7 duration-[0.2s]
+          hover:bg-ourBlack/70'
             >
               <div>시작하기</div>
               <div>→</div>
@@ -55,8 +55,8 @@ export default function Page() {
       <div className='pt-24 pb-16 bg-ourBlue/5'>
         <UpsideAnimation>
           <div className='text-center'>
-            <div className='mb-2 font-bold text-xl text-ourTheme'>다음과 같은 상황,</div>
-            <div className='font-bold text-4xl text-ourBlack'>한 번쯤 겪어보지 않으셨나요?</div>
+            <div className='mb-2 font-bold text-base md:text-xl lg:text-xl text-ourTheme'>다음과 같은 상황,</div>
+            <div className='font-bold text-2xl md:text-4xl lg:text-4xl text-ourBlack'>한 번쯤 겪어보지 않으셨나요?</div>
           </div>
         </UpsideAnimation>
         <div className='relative top-24 ping flex flex-col gap-5 justify-center px-5 md:px-28 md:flex-row items-center'>
@@ -67,22 +67,22 @@ export default function Page() {
         </div>
       </div>
       <div className='px-5 md:px-10 lg:px-28 pt-36 flex flex-col justify-center gap-10'>
-        <div className='text-center font-semibold text-xl mb-10'>
+        <div className='text-center font-semibold text-base md:text-xl lg:text-xl mb-10'>
           이런 문해력 고민{' '}
           <mark className='bg-ourGreen/30 mr-1'>
-            <span className='font-Ansungtangmyun text-3xl text-ourTheme'>심심한 사과</span>
+            <span className='font-Ansungtangmyun text-base md:text-3xl lg:text-3xl text-ourTheme'>심심한 사과</span>
           </mark>
           에서 해결해보세요!
         </div>
         <UpsideAnimation>
           <div className='w-full flex flex-col gap-5 md:flex-row lg:flex-row justify-center'>
             <div className='w-full  md:w-1/2 lg:w-1/2 h-80 flex flex-col gap-10 items-center md:items-start lg:items-start'>
-              <div className='text-ourBlue font-bold text-xl'>난이도별, 관심사별</div>
-              <div className='font-bold text-5xl flex flex-col gap-2'>
+              <div className='text-ourBlue font-bold text-base md:text-xl lg:text-xl'>난이도별, 관심사별</div>
+              <div className='font-bold text-3xl md:text-5xl lg:text-5xl flex flex-col gap-2'>
                 <span>매일 새로운</span>
                 <span>개인 맞춤 학습</span>
               </div>
-              <ul className='text-ourDarkGray list-disc ml-4'>
+              <ul className='text-ourDarkGray list-disc ml-4 text-sm md:text-base lg:text-base'>
                 <li>학습 유형, 학습 요소, 수준에 따른 학습 추천</li>
                 <li>인지 능력, 추론 능력 등을 기를 수 있는 다양한 문제</li>
               </ul>
@@ -99,13 +99,13 @@ export default function Page() {
           <div className='w-full  md:w-1/2 lg:w-1/2'>
             <RightSideAnimation>
               <div className='w-full h-80 flex flex-col gap-10 items-center md:items-start lg:items-start'>
-                <div className='text-ourBlue font-bold text-xl'>나의 요약 실력</div>
-                <div className='font-bold text-5xl flex flex-col gap-2'>
+                <div className='text-ourBlue font-bold text-base md:text-xl lg:text-xl'>나의 요약 실력</div>
+                <div className='font-bold text-3xl md:text-5xl lg:text-5xl flex flex-col gap-2'>
                   <span>짧은 글을 읽고 요약하고</span>
                   <span>모범 답안과의</span>
                   <span>유사도를 확인하세요</span>
                 </div>
-                <ul className='text-ourDarkGray list-disc ml-4'>
+                <ul className='text-ourDarkGray list-disc ml-4 text-sm md:text-base lg:text-base'>
                   <li>AI가 요약한 모범 답안 확인</li>
                   <li>내 답안과 모범 답안과의 유사도 분석</li>
                 </ul>
@@ -116,12 +116,12 @@ export default function Page() {
         <UpsideAnimation>
           <div className='w-full flex flex-col gap-5 md:flex-row lg:flex-row justify-center'>
             <div className='w-full  md:w-1/2 lg:w-1/2 h-80 flex flex-col gap-10 items-center md:items-start lg:items-start'>
-              <div className='text-ourBlue font-bold text-xl'>모든 학년별, 수준별</div>
-              <div className='font-bold text-5xl flex flex-col gap-2'>
+              <div className='text-ourBlue font-bold text-base md:text-xl lg:text-xl'>모든 학년별, 수준별</div>
+              <div className='font-bold text-3xl md:text-5xl lg:text-5xl  flex flex-col gap-2'>
                 <span>다양한 주제 지문들로</span>
                 <span>읽기 능력 상승!</span>
               </div>
-              <ul className='text-ourDarkGray list-disc ml-4'>
+              <ul className='text-ourDarkGray list-disc ml-4 text-sm md:text-base lg:text-base'>
                 <li>고등 수준 5개 대주제의 비문학 지문</li>
                 <li>다양한 문제 유형들의 실시간 피드백과 해설</li>
               </ul>
@@ -138,12 +138,12 @@ export default function Page() {
           <div className='w-full  md:w-1/2 lg:w-1/2'>
             <RightSideAnimation>
               <div className='w-full h-80 flex flex-col gap-10 items-center md:items-start lg:items-start'>
-                <div className='text-ourBlue font-bold text-xl'>매달 성장하는 나</div>
-                <div className='font-bold text-5xl flex flex-col gap-2'>
+                <div className='text-ourBlue font-bold text-base md:text-xl lg:text-xl'>매달 성장하는 나</div>
+                <div className='font-bold text-3xl md:text-5xl lg:text-5xl flex flex-col gap-2'>
                   <span>향상된 나의 능력치와</span>
                   <span>매달 학습 기록 확인</span>
                 </div>
-                <ul className='text-ourDarkGray list-disc ml-4'>
+                <ul className='text-ourDarkGray list-disc ml-4 text-sm md:text-base lg:text-base'>
                   <li>그래프를 통한 내 능력치와 강점, 약점</li>
                   <li>달력에서 바로 확인하는 내 학습 기록</li>
                 </ul>
@@ -154,12 +154,12 @@ export default function Page() {
         <UpsideAnimation>
           <div className='w-full flex flex-col gap-5 md:flex-row lg:flex-row justify-center'>
             <div className='w-full  md:w-1/2 lg:w-1/2 h-80 flex flex-col gap-10 items-center md:items-start lg:items-start'>
-              <div className='text-ourBlue font-bold text-xl'>문해력의 기초, 어휘!</div>
-              <div className='font-bold text-5xl flex flex-col gap-2'>
+              <div className='text-ourBlue font-bold text-base md:text-xl lg:text-xl'>문해력의 기초, 어휘!</div>
+              <div className='font-bold text-3xl md:text-5xl lg:text-5xl flex flex-col gap-2'>
                 <span>대결을 통한</span>
                 <span>재밌는 단어 학습</span>
               </div>
-              <ul className='text-ourDarkGray list-disc ml-4'>
+              <ul className='text-ourDarkGray list-disc ml-4 text-sm md:text-base lg:text-base'>
                 <li>단어를 알면 문장 이해력이 올라가고</li>
                 <li>단어를 알면 표현력도 올라가요</li>
               </ul>
