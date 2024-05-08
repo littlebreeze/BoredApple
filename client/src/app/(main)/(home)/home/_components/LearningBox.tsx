9352b6dba3d741418c1de9010539b773cdbeffc8
권정훈 <dhtmxk8134@naver.com>
2024-05-08 21:33:04 +0900

    feat(fe): 메인 페이지 개발

--- client/src/app/(main)/(home)/home/_components/LearningBox.tsx
+++ client/src/app/(main)/(home)/home/_components/LearningBox.tsx
@@ -1,54 +1,64 @@
 'use client';
-import { useEffect } from 'react';
-import LearningItem1 from './LearningItem1';
+import { useEffect, useState } from 'react';
+import LearningItem from './LearningItem';
 import instance from '@/utils/interceptor';
 
-// 주제맞추기 1
-// 정독훈련 2
-// 어휘 3
-// 문장삽입 4
-// 순서맞추기 5
+export type LearningItemType = {
+  type: string;
+  level: number;
+  solved: boolean;
+};
+
 export default function LearningBox() {
-  // const data = {
-  //   status: 200,
-  //   data: [
-  //     {
-  //       type: '주제맞추기',
-  //       solved: false,
-  //       level: 1,
-  //     },
-  //     {
-  //       type: '정독훈련',
-  //       solved: false,
-  //       level: 3,
-  //     },
-  //     {
-  //       type: '순서맞추기',
-  //       solved: true,
-  //       level: 5,
-  //     },
-  //   ],
-  // };
+  const [learningItems, setLearningItems] = useState<LearningItemType[]>([]);
 
-  // const getData = () => {
-  //   return data;
-  // };
-
-  useEffect(() => {
-    console.log('함수 실행');
-    handleLoad();
-  }, []);
-
-  const handleLoad = async () => {
-    const response = await instance.get(`/study-service/problem/today`);
-    console.log(response);
+  const response = {
+    data: {
+      status: 'success',
+      data: [
+        {
+          type: '주제맞추기',
+          level: 5,
+          solved: false,
+        },
+        {
+          type: '정독훈련',
+          level: 2,
+          solved: false,
+        },
+        {
+          type: '어휘',
+          level: 3,
+          solved: true,
+        },
+      ],
+    },
   };
 
+  const getData = () => {
+    return response;
+  };
+
+  useEffect(() => {
+    const response = getData();
+    console.log(response);
+    if (response.data.status === 'success') {
+      setLearningItems(response.data.data);
+    }
+  }, []);
+
+  // const handleLoad = async () => {
+  //   const response = await instance.get(`/study-service/problem/today`);
+  //   console.log(response);
+  // };
+
   return (
-    <div className='flex h-full gap-4'>
-      <LearningItem1 />
-      <LearningItem1 />
-      <LearningItem1 />
+    <div className='flex h-full'>
+      {learningItems.map((learningItem, index) => (
+        <div className='flex-1 rounded-2xl h-full' key={index}>
+          <LearningItem key={index} learningItem={learningItem} />
+        </div>
+      ))}
     </div>
   );
 }
