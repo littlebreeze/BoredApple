130d05ceab0cf76f0b41ed41bdbd4960b5b80e89
권정훈 <dhtmxk8134@naver.com>
2024-05-08 17:39:26 +0900

    feat(fe): 시작하기 경로 수정

--- client/src/app/_common/StartButton.tsx
+++ client/src/app/_common/StartButton.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function StartButton() {
   return (
     <>
-      <Link href={'/home'}>
+      <Link href={'/login'}>
         <div
           className='text-sm px-2 md:px-7 lg:px-7 py-2 cursor-pointer bg-ourBlack rounded-full text-white duration-[0.2s]
           hover:bg-ourBlack/60'
