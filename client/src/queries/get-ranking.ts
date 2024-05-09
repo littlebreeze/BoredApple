d63f724a70df58ca56ed469377d3668d502d04c0
하재률 <wofbf2@gmail.com>
2024-05-09 13:15:47 +0900

    refactor(fe): 랭킹 조회 API react query - prefetching 삭제

---
+++ client/src/queries/get-ranking.ts
@@ -0,0 +1,20 @@
+import { IRankings } from '@/types/Ranking';
+import instance from '@/utils/interceptor';
+import { useQuery } from '@tanstack/react-query';
+
+async function getRanking() {
+  try {
+    const res = await instance.get<{ data: IRankings }>(`${process.env.NEXT_PUBLIC_API_SERVER}/game-server/rankings`);
+    console.log(res);
+    return res.data.data || null;
+  } catch (e) {
+    console.log('랭킹 조회 에러', e);
+    return null;
+  }
+}
+
+export const useGetRanking = () => {
+  const { data, isLoading } = useQuery({ queryKey: ['ranking'], queryFn: getRanking });
+
+  return { data, isLoading };
+};
--- client/src/types/Room.ts
+++ client/src/types/Room.ts
@@ -1,4 +1,4 @@
-export interface Room {
+export interface IRoom {
   roomName: string;
   isSecret: boolean;
   roomPassword?: number;
