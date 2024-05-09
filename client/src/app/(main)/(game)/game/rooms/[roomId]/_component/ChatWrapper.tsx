f9be95942e284e1c8aecec47366f2ef0cb7301e8
이지원 <dlwldnjs950@naver.com>
2024-05-09 15:57:58 +0900

    design(fe): 채팅 영역 UI

--- client/src/app/(main)/(game)/game/rooms/[roomId]/_component/ChatWrapper.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/_component/ChatWrapper.tsx
@@ -17,17 +17,17 @@ export default function ChatWrapper() {
   }, [messages]);
 
   return (
-    <div className='h-full p-3 bg-ourLightGray rounded-xl flex flex-col justify-between'>
-      <div className='h-44 flex flex-col overflow-y-scroll scrollbar-hide'>
+    <div className='h-full px-3 pt-3 pb-1 bg-ourLightGray/50 rounded-xl flex flex-col justify-between'>
+      <div className='h-44 flex flex-col overflow-y-scroll'>
         {messages.map((m, idx) => (
           <div key={idx} className='p-1 flex gap-3'>
-            <div className='text-center w-2/12'>{m.nickname}</div>
-            <div className='pl-2 w-10/12 appearance-none rounded leading-tight focus:outline-none'>{m.content}</div>
+            <div className='text-center w-2/12 border-r-2'>{m.nickname}</div>
+            <div className='pl-2 w-10/12'>{m.content}</div>
           </div>
         ))}
         <div ref={messageEndRef}></div>
       </div>
-      <div className='p-2 flex gap-3'>
+      <div className='p-2 flex gap-3 border-t-2 bg-ourDarkGray/10 rounded-md'>
         <label className='text-center w-2/12'>닉네임닉</label>
         <input
           className='px-2 w-10/12 appearance-none rounded leading-tight focus:outline-none'
