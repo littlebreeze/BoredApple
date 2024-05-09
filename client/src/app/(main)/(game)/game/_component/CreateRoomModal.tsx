52fd444ae4e3149705131362739201514e55f205
하재률 <wofbf2@gmail.com>
2024-05-09 16:20:47 +0900

    refactor(fe): 방생성 모달 path 재설정

--- client/src/app/(main)/(game)/game/_component/CreateRoomModal.tsx
+++ client/src/app/(main)/(game)/game/_component/CreateRoomModal.tsx
@@ -31,7 +31,7 @@ export default function CreateRoomModal() {
       const res = await instance.post(`${process.env.NEXT_PUBLIC_API_SERVER}/game-service/rooms`, newRoom);
       const newRoomId = res.data.data.roomId;
       resetState();
-      router.replace(`game/rooms/${newRoomId}`);
+      router.replace(`/game/rooms/${newRoomId}`);
     } catch (e) {
       console.error('방 생성 중 에러가 발생했습니다.', e);
       alert('방 생성 중 에러가 발생했습니다.');
