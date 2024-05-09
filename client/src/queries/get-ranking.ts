52146e17bd1f60e36b53468da32a24cccbd94a36
하재률 <wofbf2@gmail.com>
2024-05-09 13:23:14 +0900

    refactor(fe): path 오타 수정

--- client/src/queries/get-ranking.ts
+++ client/src/queries/get-ranking.ts
@@ -4,8 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 
 async function getRanking() {
   try {
-    const res = await instance.get<{ data: IRankings }>(`${process.env.NEXT_PUBLIC_API_SERVER}/game-server/rankings`);
-    console.log(res);
+    const res = await instance.get<{ data: IRankings }>(`${process.env.NEXT_PUBLIC_API_SERVER}/game-service/rankings`);
     return res.data.data || null;
   } catch (e) {
     console.log('랭킹 조회 에러', e);
