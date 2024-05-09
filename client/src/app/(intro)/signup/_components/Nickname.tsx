bc318028b79eeea20d4b9518d493f42b6d436c30
권정훈 <dhtmxk8134@naver.com>
2024-05-09 17:55:12 +0900

    feat(fe): uri 수정

--- client/src/app/(intro)/signup/_components/Nickname.tsx
+++ client/src/app/(intro)/signup/_components/Nickname.tsx
@@ -12,7 +12,7 @@ export default function Nickname() {
   const createNickname = async () => {
     console.log(process.env.NEXT_PUBLIC_API_SERVER);
     try {
-      await instance.post(`https://k10a508.p.ssafy.io:8081/user-service/nickname`, {
+      await instance.post(`/user-service/nickname`, {
         nickname: nickname,
       });
     } catch (error) {
