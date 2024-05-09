d3d015a961f0c03831010a071ce16268d80c84c7
이종재 <ljj452@naver.com>
2024-05-09 11:22:14 +0900

    fix(be): 오류방지 수정

--- study-service/src/main/java/com/a508/studyservice/dto/response/monthResponse.java
+++ study-service/src/main/java/com/a508/studyservice/dto/response/monthResponse.java
@@ -1,14 +1,14 @@
-package com.a508.studyservice.dto.response;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class monthResponse {
-
-
-}
+// package com.a508.studyservice.dto.response;
+//
+// import lombok.AllArgsConstructor;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
+// import org.springframework.format.annotation.DateTimeFormat;
+//
+// @Data
+// @NoArgsConstructor
+// @AllArgsConstructor
+// public class monthResponse {
+//
+//
+// }
