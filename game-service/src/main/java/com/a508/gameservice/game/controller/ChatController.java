6b8ff82cb96e2816abef4d1cf1864e6a38a12a4f
최정윤 <janet971015@naver.com>
2024-05-12 00:38:39 +0900

    feat(be): 문제 관련 MessageMapping 구현

--- game-service/src/main/java/com/a508/gameservice/game/controller/ChatController.java
+++ game-service/src/main/java/com/a508/gameservice/game/controller/ChatController.java
@@ -3,6 +3,10 @@ package com.a508.gameservice.game.controller;
 import com.a508.gameservice.game.data.ChatMessageReq;
 import com.a508.gameservice.game.data.ChatMessageRes;
 import com.a508.gameservice.game.data.MessageType;
+import com.a508.gameservice.game.service.GameRoomService;
+import com.a508.gameservice.game.service.SchedulerService;
+import com.a508.gameservice.game.service.GameSchedulerManageService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -10,7 +14,11 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor
 public class ChatController {
+
+    private final GameSchedulerManageService gameSchedulerManageService;
+    private final GameRoomService gameRoomService;
 
     @MessageMapping("/ws/rooms/{roomId}/send")
     @SendTo("/topic/chat/rooms/{roomId}")
@@ -23,6 +31,8 @@ public class ChatController {
             writer = chatMessage.getSender();
         } else if (chatMessage.getType() == MessageType.CORRECT) {
             content = chatMessage.getSender() + "님이 정답을 맞히셨습니다.";
+            SchedulerService service=gameSchedulerManageService.getGameScheduler(roomId);
+            service.stopTask();
         } else if (chatMessage.getType() == MessageType.EXIT) {
             content = chatMessage.getSender() + "님이 퇴장하셨습니다.";
         }
@@ -33,6 +43,20 @@ public class ChatController {
                 .writer(writer)
                 .target(chatMessage.getSenderId())
                 .build();
+    }
+
+    @MessageMapping("/ws/quiz/rooms/{roomId}/send")
+    public void sendQuiz(@DestinationVariable Integer roomId, @Payload String message) {
+        if (message.equals("START")) {
+           //방정보 게임 중
+            gameRoomService.updateIsStarted(roomId);
+        }else if(message.equals("ROUND")){
+            SchedulerService service=gameSchedulerManageService.getGameScheduler(roomId);
+            service.startTask();
+        }else if (message.equals("END")){
+            //방정보 게임 중 X
+            gameRoomService.updateIsStarted(roomId);
+        }
     }
 
 }
