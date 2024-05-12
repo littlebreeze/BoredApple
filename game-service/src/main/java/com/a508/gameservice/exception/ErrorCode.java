e59c1ff69c3818e3c0a605651811960f4629d798
최정윤 <janet971015@naver.com>
2024-05-12 09:07:40 +0900

    feat(be): ErrorCode 추가

--- game-service/src/main/java/com/a508/gameservice/exception/ErrorCode.java
+++ game-service/src/main/java/com/a508/gameservice/exception/ErrorCode.java
@@ -8,9 +8,9 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorCode {
 
     //Game
-    PLAYER_IS_FULL_ERROR(404, "GME001", "제한 인원을 초과했습니다.");
-
-
+    PLAYER_IS_FULL_ERROR(404, "GME001", "제한 인원을 초과했습니다."),
+    ROOM_IS_NOT_EXIST(404, "GME002", "해당 방이 존재하지 않습니다"),
+    RESULT_IS_EMPTY(400, "GME003", "결과값이 없습니다.");
 
     private final int status;
 
