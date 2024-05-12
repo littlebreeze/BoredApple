3eab348f73654edce2038c97b0df8ab2577a70fb
최정윤 <janet971015@naver.com>
2024-05-12 17:23:23 +0900

    feat(be): 문제 랜덤 배정 로직 구현

--- game-service/src/main/java/com/a508/gameservice/game/controller/ChatController.java
+++ game-service/src/main/java/com/a508/gameservice/game/controller/ChatController.java
@@ -4,8 +4,8 @@ import com.a508.gameservice.game.data.ChatMessageReq;
 import com.a508.gameservice.game.data.ChatMessageRes;
 import com.a508.gameservice.game.data.MessageType;
 import com.a508.gameservice.game.service.GameRoomService;
-import com.a508.gameservice.game.service.SchedulerService;
 import com.a508.gameservice.game.service.GameSchedulerManageService;
+import com.a508.gameservice.game.service.SchedulerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -26,12 +26,12 @@ public class ChatController {
         String content = chatMessage.getMessage();
         String writer = "심심한 사과";
         if (chatMessage.getType() == MessageType.ENTER) {
-            content = chatMessage.getSender() + "님이 입장하셨습니다.";
+            content = chatMessage.getSender();
         } else if (chatMessage.getType() == MessageType.TALK) {
             writer = chatMessage.getSender();
         } else if (chatMessage.getType() == MessageType.CORRECT) {
             content = chatMessage.getSender() + "님이 정답을 맞히셨습니다.";
-            SchedulerService service=gameSchedulerManageService.getGameScheduler(roomId);
+            SchedulerService service = gameSchedulerManageService.getGameScheduler(roomId);
             service.stopTask();
         } else if (chatMessage.getType() == MessageType.EXIT) {
             content = chatMessage.getSender() + "님이 퇴장하셨습니다.";
@@ -48,12 +48,14 @@ public class ChatController {
     @MessageMapping("/ws/quiz/rooms/{roomId}/send")
     public void sendQuiz(@DestinationVariable Integer roomId, @Payload String message) {
         if (message.equals("START")) {
-           //방정보 게임 중
+            //방정보 게임 중
             gameRoomService.updateIsStarted(roomId);
-        }else if(message.equals("ROUND")){
-            SchedulerService service=gameSchedulerManageService.getGameScheduler(roomId);
-            service.startTask();
-        }else if (message.equals("END")){
+            SchedulerService service = gameSchedulerManageService.getGameScheduler(roomId);
+            service.getQuizList();
+        } else if (message.equals("ROUND")) {
+            SchedulerService service = gameSchedulerManageService.getGameScheduler(roomId);
+            service.startRound();
+        } else if (message.equals("END")) {
             //방정보 게임 중 X
             gameRoomService.updateIsStarted(roomId);
         }
