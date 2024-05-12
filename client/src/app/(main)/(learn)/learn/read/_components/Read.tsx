7093c39af220683994cf65979eec2f988767aff9
권정훈 <righthun.kwon@gmail.com>
2024-05-12 15:00:42 +0900

    feat(fe): 정독훈련 구현 완료

--- client/src/app/(main)/(learn)/learn/read/_components/Read.tsx
+++ client/src/app/(main)/(learn)/learn/read/_components/Read.tsx
@@ -17,15 +17,17 @@ export default function Read() {
   const [selected, setSelected] = useState<number | null>(null);
   const [userAnswer, setUserAnswer] = useState<(number | null)[]>([]);
   const [problemId, setProblemId] = useState<(number | null)[]>([]);
+  const [start, setStart] = useState(false);
+  const [currSentence, setCurrSentence] = useState(0);
 
   const beginTime = useRef<number>(0);
-  const currWordProblem = problems[problemIndex];
+  const currProblem = problems[problemIndex];
 
   // 문제 풀이 시간 설정
   // 문제 데이터 가져오기
   useEffect(() => {
     beginTime.current = Date.now();
-    getWordData();
+    getReadData();
   }, []);
 
   // 서버에 푼 문제 데이터 요청 전송
@@ -53,39 +55,69 @@ export default function Read() {
     }
   }, [submit]);
 
-  const getWordData = async () => {
+  // 3초마다 문장을 바꿔서 보여주는 함수
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout;
+
+    if (start) {
+      intervalId = setInterval(() => {
+        setCurrSentence((prevSentence) => {
+          if (prevSentence === currProblem.content.split('|').length - 1) {
+            clearInterval(intervalId);
+            return prevSentence;
+          }
+          return prevSentence + 1;
+        });
+      }, 5000);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [start, currProblem]);
+
+  // 문제 데이터 가져오기
+  const getReadData = async () => {
     try {
       const response = await instance.get(`/study-service/problem/intensive`);
-      console.log(response.data.data);
       setProblems(response.data.data);
     } catch (error) {
       // error
     }
   };
 
+  // 다음 버튼 클릭
   const handleNextClick = () => {
     if (selected !== null) {
       setUserAnswer((prevMyAnswer) => [...prevMyAnswer, selected]);
-      setProblemId((prevProblemId) => [...prevProblemId, currWordProblem.problemId]);
+      setProblemId((prevProblemId) => [...prevProblemId, currProblem.problemId]);
       setProgress((prevProgress) => prevProgress + 1);
       setProblemIndex((prevIndex) => prevIndex + 1);
       setSelected(null);
+      setStart(false);
     }
   };
 
-  const handleOptionClick = (option: number) => {
-    setSelected(option);
-  };
-
+  // 학습 완료 버튼 클릭
   const handleFinishClick = () => {
     setUserAnswer((prevMyAnswer) => [...prevMyAnswer, selected]);
-    setProblemId((prevProblemId) => [...prevProblemId, currWordProblem.problemId]);
+    setProblemId((prevProblemId) => [...prevProblemId, currProblem.problemId]);
     setSubmit(true);
+  };
+
+  // 시작 버튼 클릭
+  const handleStartClick = () => {
+    setStart(true);
+    setCurrSentence(0);
+  };
+
+  // 선택지 클릭
+  const handleOptionClick = (option: number) => {
+    setSelected(option);
   };
 
   return (
     <div>
       <div>
+        {/* 상태 바 */}
         <div>
           {progress === 1 ? (
             <div className='flex gap-3'>
@@ -107,6 +139,8 @@ export default function Read() {
             </div>
           )}
         </div>
+
+        {/* 문제 */}
         <div className='py-4'></div>
         <div className='flex'>
           <div className='mr-2'>정독 훈련</div>
@@ -116,49 +150,70 @@ export default function Read() {
           </div>
         </div>
         <div className='py-1'></div>
-        <div>각 문장을 정확히 끊어 읽고 문제를 풀어보세요. 준비됐다면 시작 버튼을 눌러주세요!</div>
+        <div>각 문장을 정확히 끊어 읽고 가장 적절한 선택지를 고르세요. 준비됐다면 시작 버튼을 눌러주세요!</div>
         <div className='py-2'></div>
 
-        {currWordProblem && (
+        {/* 지문 및 선택지 */}
+        {currProblem && (
           <div>
             <div className='flex gap-2'>
-              <div className='p-4 h-fit flex-1 font-Batang select-none'>{currWordProblem.content}</div>
-              <div>
-                <div className='py-12'></div>
-                <div className='w-96 bg-white rounded-xl p-4'>
-                  <div
-                    className={`cursor-pointer flex items-center p-2 m-1 rounded-xl ${
-                      selected === 1 ? 'bg-black text-white' : 'bg-[#f2f2f2]'
-                    } `}
-                    onClick={() => handleOptionClick(1)}
+              {!start ? (
+                // 시작 버튼
+                <div className='flex-1'>
+                  <button
+                    className='mt-2 cursor-pointer bg-black p-4 rounded-xl h-10 flex justify-center items-center text-white'
+                    onClick={handleStartClick}
                   >
-                    <Image className='w-4 h-4 mr-2' src={selected === 1 ? checked : unchecked} alt='선택' />
-                    <span>{currWordProblem.option1}</span>
+                    ▶ &nbsp;&nbsp;시작
+                  </button>
+                </div>
+              ) : (
+                // 지문 및 선택지
+                <div>
+                  <div>
+                    <div className='p-4 h-fit flex-1 font-Batang select-none'>
+                      {currProblem.content.split('|')[currSentence]} &nbsp;
+                    </div>
                   </div>
-                  <div
-                    className={`cursor-pointer flex items-center p-2 m-1 rounded-xl ${
-                      selected === 2 ? 'bg-black text-white' : 'bg-[#f2f2f2]'
-                    } `}
-                    onClick={() => handleOptionClick(2)}
-                  >
-                    <Image className='w-4 h-4 mr-2' src={selected === 2 ? checked : unchecked} alt='선택' />
-                    <span>{currWordProblem.option2}</span>
-                  </div>
-                  <div
-                    className={`cursor-pointer flex items-center p-2 m-1 rounded-xl ${
-                      selected === 3 ? 'bg-black text-white' : 'bg-[#f2f2f2]'
-                    } `}
-                    onClick={() => handleOptionClick(3)}
-                  >
-                    <Image className='w-4 h-4 mr-2' src={selected === 3 ? checked : unchecked} alt='선택' />
-                    <span>{currWordProblem.option3}</span>
+                  <div>
+                    <div className='py-12'></div>
+                    <div className='w-96 bg-white rounded-xl p-4'>
+                      <div
+                        className={`cursor-pointer flex items-center p-2 m-1 rounded-xl ${
+                          selected === 1 ? 'bg-black text-white' : 'bg-[#f2f2f2]'
+                        } `}
+                        onClick={() => handleOptionClick(1)}
+                      >
+                        <Image className='w-4 h-4 mr-2' src={selected === 1 ? checked : unchecked} alt='선택' />
+                        <span>{currProblem.option1}</span>
+                      </div>
+                      <div
+                        className={`cursor-pointer flex items-center p-2 m-1 rounded-xl ${
+                          selected === 2 ? 'bg-black text-white' : 'bg-[#f2f2f2]'
+                        } `}
+                        onClick={() => handleOptionClick(2)}
+                      >
+                        <Image className='w-4 h-4 mr-2' src={selected === 2 ? checked : unchecked} alt='선택' />
+                        <span>{currProblem.option2}</span>
+                      </div>
+                      <div
+                        className={`cursor-pointer flex items-center p-2 m-1 rounded-xl ${
+                          selected === 3 ? 'bg-black text-white' : 'bg-[#f2f2f2]'
+                        } `}
+                        onClick={() => handleOptionClick(3)}
+                      >
+                        <Image className='w-4 h-4 mr-2' src={selected === 3 ? checked : unchecked} alt='선택' />
+                        <span>{currProblem.option3}</span>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
+              )}
             </div>
           </div>
         )}
 
+        {/* 버튼 */}
         <div className='flex border-t border-ourGray absolute bottom-4 w-full justify-end'>
           {progress >= 3 ? (
             <button
--- client/src/app/(main)/(learn)/learn/read/_components/ReadResult.tsx
+++ client/src/app/(main)/(learn)/learn/read/_components/ReadResult.tsx
@@ -10,22 +10,66 @@ import { IBasicProblem, BasicProblemResponse } from '@/types/Problem';
 
 export default function ReadResult() {
   const router = useRouter();
-  const [wordProblems, setWordProblems] = useState<BasicProblemResponse>([]);
-  const [wordProblemIndex, setWordProblemIndex] = useState(0);
-
+  const [problems, setProblems] = useState<BasicProblemResponse>([]);
+  const [problemIndex, setProblemIndex] = useState(0);
   const [progress, setProgress] = useState(1);
-  const [selected, setSelected] = useState<number | null>(null);
 
-  const currWordProblem = wordProblems[wordProblemIndex];
+  const currProblem = problems[problemIndex];
 
   useEffect(() => {
-    getWordData();
+    getReadData();
   }, []);
 
-  const getWordData = async () => {
+  // 임시 데이터
+  const temp = {
+    status: 'success',
+    data: [
+      {
+        content:
+          '중력은 모든 물체에 작용하는 자연 법칙이다. |  이것은 물체가 지구 중심으로 떨어지는 이유이기도 하다. |  또한 중력은 우주의 다양한 현상에 영향을 미친다. |  행성들의 공전 경로부터 별의 운동까지 중력이 관여한다. | ',
+        option1: '중력은 지구만의 현상이다.',
+        option2: '중력은 모든 물체에 작용하는 자연 법칙이다.',
+        option3: '중력은 인간이 만든 것이다.',
+        userAnswer: 2,
+        answer: 2,
+        category: '과학',
+        type: '정독훈련',
+        problemId: 3,
+        correct: false,
+      },
+      {
+        content:
+          '태양계는 여러 행성과 소행성, 위성 등으로 구성되어 있다. |  각 천체들은 자신만의 공전 궤도를 가지며 태양 주위를 돌고 있다. |  이러한 천체들의 운동은 우주의 원리를 이해하는 데 중요한 단서를 제공한다. | ',
+        option1: '태양은 지구 중심으로 공전한다.',
+        option2: '태양계는 여러 행성과 소행성, 위성 등으로 구성되어 있다.',
+        option3: '행성들의 운동은 무질서하다.',
+        userAnswer: 2,
+        answer: 3,
+        category: '과학',
+        type: '정독훈련',
+        problemId: 8,
+        correct: false,
+      },
+      {
+        content:
+          '태양계는 여러 행성과 소행성, 위성 등으로 구성되어 있다. |  각 천체들은 자신만의 공전 궤도를 가지며 태양 주위를 돌고 있다. |  이러한 천체들의 운동은 우주의 원리를 이해하는 데 중요한 단서를 제공한다. | ',
+        option1: '태양은 지구 중심으로 공전한다.',
+        option2: '태양계는 여러 행성과 소행성, 위성 등으로 구성되어 있다.',
+        option3: '행성들의 운동은 무질서하다.',
+        userAnswer: 2,
+        answer: 1,
+        category: '과학',
+        type: '정독훈련',
+        problemId: 13,
+        correct: false,
+      },
+    ],
+  };
+
+  const getReadData = async () => {
     try {
-      const response = await instance.get(`/study-service/problem/voca`);
-      setWordProblems(response.data.data);
+      const response = await instance.get(`/study-service/problem/intensive`);
+      setProblems(response.data.data);
     } catch (error) {
       // error
     }
@@ -33,7 +77,7 @@ export default function ReadResult() {
 
   const handleNextClick = () => {
     setProgress((prevProgress) => prevProgress + 1);
-    setWordProblemIndex((prevIndex) => prevIndex + 1);
+    setProblemIndex((prevIndex) => prevIndex + 1);
   };
 
   const handleFinishClick = () => {
@@ -41,12 +85,12 @@ export default function ReadResult() {
   };
 
   const optionTextColor = (option: number) => {
-    if (option === currWordProblem.answer && option === currWordProblem.userAnswer) {
+    if (option === currProblem.answer && option === currProblem.userAnswer) {
       return 'text-ourBlue font-semibold';
     } else {
-      if (option === currWordProblem.answer) {
+      if (option === currProblem.answer) {
         return 'text-ourBlue font-semibold';
-      } else if (option === currWordProblem.userAnswer) {
+      } else if (option === currProblem.userAnswer) {
         return 'text-ourRed line-through font-semibold';
       } else {
         return '';
@@ -55,13 +99,13 @@ export default function ReadResult() {
   };
 
   const optionImage = (option: number) => {
-    if (currWordProblem.userAnswer === option) {
-      if (currWordProblem.userAnswer === currWordProblem.answer) {
+    if (currProblem.userAnswer === option) {
+      if (currProblem.userAnswer === currProblem.answer) {
         return checkTrue;
       } else {
         return checkFalse;
       }
-    } else if (option === currWordProblem.answer) {
+    } else if (option === currProblem.answer) {
       return checkTrue;
     } else {
       return unchecked;
@@ -71,6 +115,7 @@ export default function ReadResult() {
   return (
     <div>
       <div>
+        {/* 상태 바 */}
         <div>
           {progress === 1 ? (
             <div className='flex gap-3'>
@@ -92,6 +137,8 @@ export default function ReadResult() {
             </div>
           )}
         </div>
+
+        {/* 문제 */}
         <div className='py-4'></div>
         <div className='flex'>
           <div className='mr-2'>정독 훈련</div>
@@ -101,13 +148,14 @@ export default function ReadResult() {
           </div>
         </div>
         <div className='py-1'></div>
-        <div>다음 문장의 의미에 부합하는 적절한 어휘를 고르시오.</div>
+        <div>각 문장을 정확히 끊어 읽고 가장 적절한 선택지를 고르세요. 준비됐다면 시작 버튼을 눌러주세요!</div>
         <div className='py-2'></div>
 
-        {currWordProblem && (
+        {/* 지문 및 선택지 */}
+        {currProblem && (
           <div>
             <div className='flex gap-2'>
-              <div className='p-4 h-fit flex-1 font-Batang'>{currWordProblem.content}</div>
+              <div className='p-4 h-fit flex-1 font-Batang'> {currProblem.content.replace(/\|/g, '')}</div>
               <div>
                 <div className='py-12'></div>
                 <div className='w-96 bg-white rounded-xl p-4'>
@@ -117,7 +165,7 @@ export default function ReadResult() {
                       className={`flex items-center p-2 m-1 rounded-xl ${optionTextColor(option)} bg-[#f2f2f2]`}
                     >
                       <Image className='w-4 h-4 mr-2' src={optionImage(option)} alt='선택' />
-                      <span>{currWordProblem[`option${option}` as keyof IBasicProblem]}</span>
+                      <span>{currProblem[`option${option}` as keyof IBasicProblem]}</span>
                     </div>
                   ))}
                 </div>
@@ -126,6 +174,7 @@ export default function ReadResult() {
           </div>
         )}
 
+        {/* 버튼 */}
         <div className='flex border-t border-ourGray absolute bottom-4 w-full justify-end'>
           {progress >= 3 ? (
             <button
