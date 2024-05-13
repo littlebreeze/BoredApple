a96cabb6754bb93a9848dda622a9f637beecb383
이종재 <ljj452@naver.com>
2024-05-13 14:02:53 +0900

    feat(be): 유사도 기능, ErrorCode 추가

---
+++ study-service/src/main/java/com/a508/studyservice/dto/response/SimilarityResponse.java
@@ -0,0 +1,8 @@
+package com.a508.studyservice.dto.response;
+
+import lombok.Data;
+
+@Data
+public class SimilarityResponse {
+	int ratio;
+}
--- study-service/src/main/java/com/a508/studyservice/feign/SimilarityServiceFeignClient.java
+++ study-service/src/main/java/com/a508/studyservice/feign/SimilarityServiceFeignClient.java
@@ -1,14 +1,18 @@
 package com.a508.studyservice.feign;
 
-import com.a508.studyservice.dto.request.FeignEssaySimilarityRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "similar-service" , url = "http://k10a508.p.ssafy.io/similarity-service")
+import com.a508.studyservice.dto.response.SimilarityResponse;
+
+import feign.Headers;
+
+@FeignClient(name = "similar-service" , url = "http://k10a508.p.ssafy.io:8082")
 public interface SimilarityServiceFeignClient {
 
     //주제 찾기 유사도 받아오기
-    @PostMapping()
-    double essaySimilarity(@RequestBody FeignEssaySimilarityRequest essayDto );
+    @PostMapping("/sim")
+    @Headers("Content-Type: application/json")
+    SimilarityResponse essaySimilarity(@RequestBody String[] essayDto );
 }
