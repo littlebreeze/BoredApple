f88387733752e1c64dabf913dd7d04a2be2bffca
최정윤 <janet971015@naver.com>
2024-05-13 15:26:08 +0900

    feat(be): 게임 결과 api 구현

--- game-service/src/main/java/com/a508/gameservice/game/controller/GameRoomController.java
+++ game-service/src/main/java/com/a508/gameservice/game/controller/GameRoomController.java
@@ -8,6 +8,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 
 @RequiredArgsConstructor
 @RestController
@@ -52,11 +56,22 @@ public class GameRoomController {
     }
 
     /**
-     * 마이페이지 전적조회
+     * 마이페이지 전적조회 feign
      */
     @GetMapping("/record")
     public MyBattleRecordRes getMyRecord(@RequestHeader(value = "Authorization") String token) {
         return gameRoomService.getMyRecord(token);
+    }
+
+    /**
+     게임 결과
+     */
+    @PostMapping("/results")
+    public SuccessResponse<Map<String, List<ResultRes>>> getResult(@RequestBody ResultListReq resultListReq) {
+        Map<String, List<ResultRes>> data = new TreeMap<>();
+        data.put("ResultList", gameRoomService.getResult(resultListReq));
+        return new SuccessResponse<>(data);
+
     }
 
 
