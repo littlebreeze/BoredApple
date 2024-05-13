d97e35ed8452214a66e096d9d43e97d305575206
최정윤 <janet971015@naver.com>
2024-05-13 21:46:59 +0900

    feat(be): 퇴장 api 구현

--- game-service/src/main/java/com/a508/gameservice/game/repository/RoomPlayerRepository.java
+++ game-service/src/main/java/com/a508/gameservice/game/repository/RoomPlayerRepository.java
@@ -4,6 +4,8 @@ import com.a508.gameservice.game.domain.RoomPlayer;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Repository
@@ -30,5 +32,23 @@ public class RoomPlayerRepository {
         String key = ROOM_PLAYER_HASH_KEY + roomId;
         return redisTemplate.opsForHash().entries(key);
     }
+
+    public void removePlayerToRoom(String roomId,int userId){
+        String key = ROOM_PLAYER_HASH_KEY + roomId;
+        String userIdStr = String.valueOf(userId);
+        redisTemplate.opsForHash().delete(key,userIdStr);
+    }
+
+    public List<RoomPlayer> getRoomPlayers(String roomId){
+        List<RoomPlayer> roomPlayers=new ArrayList<>();
+        String key = ROOM_PLAYER_HASH_KEY + roomId;
+        for (Object userId:redisTemplate.opsForHash().entries(key).keySet()){
+            RoomPlayer roomPlayer= (RoomPlayer) redisTemplate.opsForHash().get(key,userId);
+            roomPlayers.add(roomPlayer);
+        }
+        return roomPlayers;
+    }
+
+
 
 }
