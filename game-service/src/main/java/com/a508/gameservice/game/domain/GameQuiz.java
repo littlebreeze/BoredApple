105bb607a9430c649f8b41dd1563a6d4705ca66c
최정윤 <janet971015@naver.com>
2024-05-13 13:05:35 +0900

    refactor(be): autoincrement 추가

--- game-service/src/main/java/com/a508/gameservice/game/domain/GameQuiz.java
+++ game-service/src/main/java/com/a508/gameservice/game/domain/GameQuiz.java
@@ -1,8 +1,6 @@
 package com.a508.gameservice.game.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -13,6 +11,7 @@ import lombok.*;
 public class GameQuiz {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
 
     @Column(nullable = false)
