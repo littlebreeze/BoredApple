1ef6234c933ff11ca174e5939694c5b2f32c3d9f
이지원 <dlwldnjs950@naver.com>
2024-05-15 02:21:05 +0900

    design(fe): 게임에서 학습페이지 이동 버튼

--- client/src/app/(main)/(game)/game/layout.tsx
+++ client/src/app/(main)/(game)/game/layout.tsx
@@ -1,5 +1,6 @@
 import RQProvider from '@/queries/RQProvider';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Layout({
   children,
@@ -19,7 +20,7 @@ export default function Layout({
         alt='게임배경'
       />
       <div className='relative z-10'>
-        <div className='px-5 '>
+        <div className='px-5'>
           <div className='w-[300px] mx-auto pt-7 pb-3'>
             <Image
               className='w-full drop-shadow-[2px_2px_2px_rgba(0,0,0,0.2)] z-20 relative'
@@ -36,6 +37,16 @@ export default function Layout({
           </RQProvider>
         </div>
       </div>
+      <Link href={'/home'} className='absolute bottom-10 right-10 z-30 w-16 h-16 duration-150 hover:w-20 hover:h-20'>
+        <Image
+          className='w-full drop-shadow-[2px_2px_2px_rgba(0,0,0,0.3)] z-20 relative'
+          src='/game/icon-image.svg'
+          loading='eager'
+          fill
+          alt='학습페이지로'
+          title='학습페이지로'
+        />
+      </Link>
     </div>
   );
 }
