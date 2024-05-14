3bc642c29df2bca86d54ce7dae7eb1ea4664165d
최정윤 <janet971015@naver.com>
2024-05-14 09:48:24 +0900

    feat(be): 방장 정보 전송

--- game-service/src/main/java/com/a508/gameservice/game/data/MessageType.java
+++ game-service/src/main/java/com/a508/gameservice/game/data/MessageType.java
@@ -2,7 +2,7 @@ package com.a508.gameservice.game.data;
 
 
 public enum MessageType {
-    CORRECT, ENTER, EXIT, TALK, QUIZ, ANSWER, START, END
+    CORRECT, ENTER, EXIT, TALK, QUIZ, ANSWER, START, END, MANAGER
 }
 
 
