b148115a2f9d4daac3823056699457df11d05aac
이지원 <dlwldnjs950@naver.com>
2024-05-14 15:26:33 +0900

    design(fe): 캘린더 색상 설명

--- client/src/app/(main)/(home)/mypage/record/_components/MonthlyRecord.tsx
+++ client/src/app/(main)/(home)/mypage/record/_components/MonthlyRecord.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import CalendarComponent from './CalendarComponent';
 import RecordList from './RecordList';
 
@@ -9,6 +8,28 @@ export default function MonthlyRecord() {
       <div className='flex justify-center'>
         <div>
           <CalendarComponent />
+          <div className='flex gap-3 mt-4 ml-3'>
+            <div className='flex items-center gap-1'>
+              <div className='h-[10px] w-[10px] rounded-full bg-ourBlue'></div>
+              <div className='text-sm text-ourDarkGray'>모두 완료</div>
+            </div>
+            <div className='flex items-center gap-1'>
+              <div className='h-[10px] w-[10px] rounded-full bg-ourBlue/50'></div>
+              <div className='text-sm text-ourDarkGray'>2개</div>
+            </div>
+            <div className='flex items-center gap-1'>
+              <div className='h-[10px] w-[10px] rounded-full bg-ourBlue/20'></div>
+              <div className='text-sm text-ourDarkGray'>1개</div>
+            </div>
+            <div className='flex items-center gap-1'>
+              <div className='h-[10px] w-[10px] rounded-full bg-ourGreen'></div>
+              <div className='text-sm text-ourDarkGray'>오늘</div>
+            </div>
+            <div className='flex items-center gap-1'>
+              <div className='h-[10px] w-[10px] rounded-full bg-ourBlack'></div>
+              <div className='text-sm text-ourDarkGray'>선택</div>
+            </div>
+          </div>
         </div>
       </div>
       <div>
