f3ce40119b130dbd4e0865c690feed85c443c29d
이종재 <ljj452@naver.com>
2024-05-14 15:22:27 +0900

    fix(be): 오늘의 학습 수정

--- study-service/src/main/java/com/a508/studyservice/controller/SolvedController.java
+++ study-service/src/main/java/com/a508/studyservice/controller/SolvedController.java
@@ -23,7 +23,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/study-service/solve")
+@RequestMapping("/solve")
 @RequiredArgsConstructor
 @Slf4j
 public class SolvedController {
