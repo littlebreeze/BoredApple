ec3121074add55898587dd2a6a7a26d821cb8899
강지헌 <wlgjs0458@naver.com>
2024-05-14 12:41:34 +0900

    feat(be): 5가지 능력 불러오기

---
+++ user-service/src/main/java/com/a508/userservice/user/service/StudyServiceClient.java
@@ -0,0 +1,16 @@
+package com.a508.userservice.user.service;
+
+import com.a508.userservice.user.data.UserAbilityRes;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "study-service")
+public interface StudyServiceClient {
+
+	@GetMapping("/solve/five")
+	public UserAbilityRes GetUserAbility(@RequestHeader(value = "Authorization") String token);
+
+	@GetMapping("/solve/five/average")
+	public UserAbilityRes GetAvgAbility();
+}
