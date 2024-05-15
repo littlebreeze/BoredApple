cbe9e18d76acd4ac720c5bee371aa8be8700b60f
권정훈 <righthun.kwon@gmail.com>
2024-05-15 13:06:01 +0900

    feat(fe): middleware 해제

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -13,11 +13,11 @@ export async function middleware(req: NextRequest) {
   // 토큰이 없을 경우 로그인 창으로 이동
 
   // 토큰이 없을 때 로그인페이지로 보냄
-  // 와! 지려 ㅆ다 접근 가능할듯? http only 에 대해 살펴보자
-  const token = req.cookies.get('refreshToken');
-  if (!token) {
-    return NextResponse.redirect(new URL('/login', req.url));
-  }
+  // 와! 지렸다 접근 가능할듯? http only 에 대해 살펴보자
+  // const token = req.cookies.get('refreshToken');
+  // if (!token) {
+  //   return NextResponse.redirect(new URL('/login', req.url));
+  // }
 
   // 요청 계속 진행
   return NextResponse.next();
