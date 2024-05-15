3ae4020dad51579636a96a25ddfb333c2a6e53a1
이종재 <149658209+JongJae2@users.noreply.github.com>
2024-05-16 05:51:52 +0900

    feat(be): kafka consumer, sse 기능 작업 중

--- study-service/src/main/java/com/a508/studyservice/controller/SSEController.java
+++ study-service/src/main/java/com/a508/studyservice/controller/SSEController.java
@@ -2,14 +2,9 @@ package com.a508.studyservice.controller;
 
 import com.a508.studyservice.kafka.StudyAlarmService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
-import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-import reactor.core.publisher.Flux;
-
-import java.time.Duration;
 
 @RestController
 @RequiredArgsConstructor
