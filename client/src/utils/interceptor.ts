f73f2e3a7775161bb577cfe30c47654af975ee0a
권정훈 <dhtmxk8134@naver.com>
2024-05-16 13:03:41 +0900

    feat(fe): access token 메모리 저장 테스트

--- client/src/utils/interceptor.ts
+++ client/src/utils/interceptor.ts
@@ -1,26 +1,12 @@
 'use client';
 import axios from 'axios';
 
-// 변수에 저장한다는 게 무엇인지?
-// 실행 중인 자바스크립트 메모리에 값이 있는 경우에는 외부에서 접근이 어렵다.
-// accessToken을 zustand persist를 활용해서 저장하면 안된다.
-// persist는 새로고침해도 값 날아가지 말라고 쓴다
-// 상태관리는 새로고침하면 값이 날라가는건데 안 날아가면 로컬스토리지 / 세션스토리지 / 쿠키에 박는 것이다.
-// 결국 이거는 여전히 local storage 쓰는 거랑 같음
-// 새로고침하는 유저는 그렇게 많지 않을 것 -> 전역으로 재발급 하면 된다.
-// 결국 그냥 로직 짜서 재발급 받아라!!!
-
-// withCredentials: true 담아주기
-
-// 스토어 만들어서 상태관리 사용하세요 그냥
-
-// 쿠키에 저장한다는 게 무엇인지?
-
 // axios 기본 인스턴스
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_SERVER,
+  withCredentials: true,
   headers: {
-    'Access-Control-Allow-Origin': 'https://j10a601.p.ssafy.io:8081', // CORS 정책을 허용할 오리진 설정
+    'Access-Control-Allow-Origin': 'https://k10a601.p.ssafy.io:8081', // CORS 정책을 허용할 오리진 설정
     'Access-Control-Allow-Credentials': 'true', // CORS 요청에서 자격 증명 정보를 허용
     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE', // CORS 요청에서 허용하는 HTTP 메소드
   },
@@ -29,6 +15,7 @@ const instance = axios.create({
 // refresh token 재생성용 axios 인스턴스
 const refreshInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_SERVER,
+  withCredentials: true,
 });
 
 // 인스턴스의 요청 인터셉터
@@ -36,7 +23,7 @@ const refreshInstance = axios.create({
 instance.interceptors.request.use(
   (config) => {
     // 요청 전달 전 미리 헤더에 엑세스 토큰 저장
-    config.headers['Authorization'] = `Bearer ${localStorage.getItem('accessToken')}`;
+    // config.headers['Authorization'] = `Bearer ${localStorage.getItem('accessToken')}`;
     return config;
   },
   (error) => {
@@ -65,16 +52,22 @@ instance.interceptors.response.use(
         // 토큰 재발급 성공 시 토큰을 다시 세팅하고 헤더에 담음
         if (response.status == 200) {
           const newAccessToken = response.data.data.accessToken;
-          localStorage.setItem('accessToken', response.data.data.accessToken);
+          // localStorage.setItem('accessToken', response.data.data.accessToken);
           localStorage.setItem('refreshToken', response.data.data.refreshToken);
           axios.defaults.headers.common['Authorization'] = `Bearer ${newAccessToken}`;
           return instance(originRequest);
         }
       } catch (error) {
         // 토큰 재발급 실패 시 로그인 요청 페이지로 이동
-        localStorage.removeItem('accessToken');
+
+        // 로컬스토리지용
+        // localStorage.removeItem('accessToken');
         localStorage.removeItem('refreshToken');
         window.location.replace('/login');
+
+        // 쿠키용
+        // localStorage.removeItem('accessToken');
+        // window.location.replace('/login');
       }
     }
     return Promise.reject(error);
@@ -86,8 +79,13 @@ const regenerateRefreshToken = async () => {
   const headers = {
     'Content-Type': 'text/plain;charset=utf-8',
   };
+
+  // 로컬스토리지용
   const refreshToken = localStorage.getItem('refreshToken');
   const response = await refreshInstance.post('/oauth/token', refreshToken, { headers: headers });
+
+  // 쿠키용
+  // const response = await refreshInstance.post('/oauth/token', {}, { headers: headers });
   return response;
 };
 
