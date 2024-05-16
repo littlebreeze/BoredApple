9e247b8ba5549ed03ccf937b3da031b73d0f35d1
이지원 <dlwldnjs950@naver.com>
2024-05-16 15:20:11 +0900

    fix(fe): useSearchParam & Suspend

--- client/src/app/(main)/(learn)/learn/summary/result/page.tsx
+++ client/src/app/(main)/(learn)/learn/summary/result/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import CloseButton from '../../_components/CloseButton';
 import InsertResult from '../_components/SummaryResult';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: '지문요약 - 오늘의 학습',
@@ -13,7 +14,9 @@ export default function Page() {
       <div className='py-1'></div>
       <CloseButton />
       <div className='py-2'></div>
-      <InsertResult />
+      <Suspense>
+        <InsertResult />
+      </Suspense>
     </div>
   );
 }
