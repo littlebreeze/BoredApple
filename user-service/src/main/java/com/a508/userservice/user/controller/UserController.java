5e7a8caaa2633ca777f5d800d20ac2a9d8f3eb66
강지헌 <wlgjs0458@naver.com>
2024-05-16 12:46:07 +0900

    fix(be): 캘린더 테스트

--- user-service/src/main/java/com/a508/userservice/user/controller/UserController.java
+++ user-service/src/main/java/com/a508/userservice/user/controller/UserController.java
@@ -116,10 +116,10 @@ public class UserController {
 		System.out.println(daysInMonth);
 		LocalDate da = LocalDate.of(date.getYear(),date.getMonth(),1);
 		System.out.println("da: " + da);
-		Object cal = studyServiceClient.GetMonthStudy(request.getHeader(AUTHORIZATION_HEADER).substring(7), da.format(formatter));
-		System.out.println(cal);
+		CalendarRes cal = studyServiceClient.GetMonthStudy(request.getHeader(AUTHORIZATION_HEADER).substring(7), da.format(formatter));
+		System.out.println(cal.getData().get(0).getSolveCnt());
 		for (int i = 0; i < daysInMonth; i++) {
-//			result.add(cal.getData().get(i).getSolveCnt());
+			result.add(cal.getData().get(i).getSolveCnt());
 		}
 
 		for(int i:result) System.out.print(i+" ");
