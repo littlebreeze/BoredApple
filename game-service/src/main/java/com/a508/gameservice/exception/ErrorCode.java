017e59d7277b5178062da938c0dfefd5af59a329
최정윤 <janet971015@naver.com>
2024-05-23 14:28:11 +0900

    refactor(be): 에러코드 수정

--- game-service/src/main/java/com/a508/gameservice/exception/ErrorCode.java
+++ game-service/src/main/java/com/a508/gameservice/exception/ErrorCode.java
@@ -10,10 +10,10 @@ public enum ErrorCode {
     //Game
     PLAYER_IS_FULL_ERROR(404, "GME001", "제한 인원을 초과했습니다."),
     ROOM_IS_NOT_EXIST(404, "GME002", "해당 방이 존재하지 않습니다"),
-    RESULT_IS_EMPTY(444, "GME003", "결과값이 없습니다."),
+    RESULT_IS_EMPTY(400, "GME003", "결과값이 없습니다."),
     GAME_IS_STARTED(404, "GME004", "해당 방의 게임이 시작되었습니다."),
-    QUICK_ENTRY_ROOM_IS_EMPTY(444, "GME005", "현재 입장 가능한 방이 없습니다."),
-   GAME_ROOM_IS_EMPTY(444, "GME006", "현재 게임 방이 없습니다.");
+    QUICK_ENTRY_ROOM_IS_EMPTY(400, "GME005", "현재 입장 가능한 방이 없습니다."),
+   GAME_ROOM_IS_EMPTY(400, "GME006", "현재 게임 방이 없습니다.");
 
     private final int status;
 
